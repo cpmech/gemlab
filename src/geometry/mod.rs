bb582517001420c391830c4b907579e1b3bebc24
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-10-29 20:37:19 +1000

    Add geometry mod

---
+++ src/geometry/mod.rs
@@ -0,0 +1,28 @@
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Circle {
+    pub center: [f64; 2],
+    pub radius: f64,
+    pub tolerance: f64,
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn circle_traits_work() {
+        let circle = Circle {
+            center: [-1.0, -1.0],
+            radius: 2.0,
+            tolerance: 1e-3,
+        };
+        let clone = circle.clone();
+        assert_eq!(circle, clone);
+        assert_eq!(
+            format!("{:?}", circle),
+            "Circle { center: [-1.0, -1.0], radius: 2.0, tolerance: 0.001 }"
+        );
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod as_array;
+mod geometry;
 pub mod shapes;
 mod structured;
 pub use crate::as_array::*;
+pub use crate::geometry::*;
 pub use crate::structured::*;
