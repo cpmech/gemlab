a99f62c0df59d1b8bea0fc609563128a5de290bb
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-11-06 17:32:53 +1000

    Improve geo defs and algos

--- src/geometry/algorithms.rs
+++ src/geometry/algorithms.rs
@@ -1,19 +1,112 @@
-/*
-// Computes the distance from point to line passing through a -> b
-pub fn dist_point_line(p, a, b *Point, tol float64, verbose bool) float64 {
-    ns := NewSegment(a, b)
-    vs := NewSegment(p, a)
-    nn := ns.Len()
-    if nn < tol { // point-point distance
-        if verbose {
-            io.Pfred("basicgeom.go: DistPointLine: __WARNING__ point-point distance too small:\n p=%v a=%v b=%v\n", p, a, b)
-        }
-        return vs.Len()
+use crate::{Point3D, StrError, Vector3D};
+
+/// Computes the distance between a point and this segment
+///
+/// ```text
+///                 c
+///                /^
+/// ⇀             / | ⇀   ⇀   ⇀
+/// x = vec(c-a) /  | q = x - p
+///             /   |       
+///            /    |
+///           a----->-------------b
+///             ⇀              ⇀
+///             p = projection(x)
+///
+///           a-------------------b
+///               ⇀
+///               n = vec(b-c)
+/// ```
+///
+/// ```text
+/// x := c - a
+/// n := b - a
+/// p := ((x ⋅ n) / (n ⋅ n)) * n
+/// q := x - p
+/// distance := norm(q)
+/// ```
+pub fn point_segment_distance_3d(a: &Point3D, b: &Point3D, c: &Point3D) -> Result<f64, StrError> {
+    let n = Vector3D::new_from_points(a, b); // b - a
+    let denominator = n.dot(&n); // n ⋅ n
+    if denominator <= f64::EPSILON {
+        return Err("segment is too short");
     }
-    n := ns.Vector(1.0 / nn)
-    v := vs.Vector(1.0)
-    s := VecDot(v, n)
-    l := VecNewAdd(1, v, -s, n) // l := v - dot(v,n) * n
-    return VecNorm(l)
+    let x = Vector3D::new_from_points(a, c); // c - a
+    let scale = x.dot(&n) / denominator; // (x ⋅ n) / (n ⋅ n)
+    let p = Vector3D::new_scaled(n.u, n.v, n.w, scale);
+    let q = x.get_subtracted(&p); // x - p
+    Ok(q.norm())
 }
-*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::SQRT_2_BY_3;
+    use russell_chk::assert_approx_eq;
+
+    #[test]
+    fn point_segment_distance_3d_fails_on_short_segment() -> Result<(), StrError> {
+        let a = Point3D { x: 0.0, y: 0.0, z: 0.0 };
+        let b = Point3D {
+            x: f64::EPSILON,
+            y: f64::EPSILON,
+            z: f64::EPSILON,
+        };
+        let c = Point3D { x: 1.0, y: 1.0, z: 1.0 };
+        assert_eq!(
+            point_segment_distance_3d(&a, &b, &c).err(),
+            Some("segment is too short")
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn point_segment_distance_3d_works() -> Result<(), StrError> {
+        let a = Point3D { x: 0.0, y: 0.0, z: 0.0 };
+        let b = Point3D { x: 1.0, y: 1.0, z: 1.0 };
+        let c = a.clone();
+        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        assert_eq!(distance, 0.0);
+
+        let c = b.clone();
+        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        assert_eq!(distance, 0.0);
+
+        let c = Point3D { x: 1.0, y: 1.0, z: 0.0 };
+        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        assert_approx_eq!(distance, SQRT_2_BY_3, 1e-15);
+
+        let c = Point3D { x: 0.0, y: 1.0, z: 1.0 };
+        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        assert_approx_eq!(distance, SQRT_2_BY_3, 1e-15);
+
+        let c = Point3D { x: 1.0, y: 0.0, z: 1.0 };
+        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        assert_approx_eq!(distance, SQRT_2_BY_3, 1e-15);
+
+        let c = Point3D { x: 0.5, y: 0.5, z: 0.5 };
+        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        assert_eq!(distance, 0.0);
+
+        let a = Point3D { x: 0.0, y: 0.0, z: 0.0 };
+        let b = Point3D { x: 1.0, y: 0.0, z: 0.0 };
+        let c = Point3D { x: 1.0, y: 0.5, z: 0.0 };
+        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        assert_eq!(distance, 0.5);
+
+        let a = Point3D { x: 0.0, y: 0.0, z: 0.0 };
+        let b = Point3D { x: 0.0, y: 1.0, z: 0.0 };
+        let c = Point3D { x: 0.5, y: 1.0, z: 0.0 };
+        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        assert_eq!(distance, 0.5);
+
+        let a = Point3D { x: 0.0, y: 0.0, z: 0.0 };
+        let b = Point3D { x: 0.0, y: 0.0, z: 1.0 };
+        let c = Point3D { x: 0.0, y: 0.5, z: 1.0 };
+        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        assert_eq!(distance, 0.5);
+        Ok(())
+    }
+}
--- src/geometry/definitions.rs
+++ src/geometry/definitions.rs
@@ -6,13 +6,6 @@ pub struct Point3D {
     pub z: f64,
 }
 
-/// Represents a directed segment from A to B in 3D
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Segment3D {
-    pub a: Point3D,
-    pub b: Point3D,
-}
-
 /// Represents a vector in 3D
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Vector3D {
@@ -21,8 +14,16 @@ pub struct Vector3D {
     pub w: f64,
 }
 
+/// Represents a directed segment from A to B in 3D
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Segment3D {
+    pub a: Point3D,
+    pub b: Point3D,
+}
+
 impl Point3D {
     /// Returns a new translated point
+    #[inline]
     pub fn get_translated(&self, dx: f64, dy: f64, dz: f64) -> Point3D {
         Point3D {
             x: self.x + dx,
@@ -32,6 +33,7 @@ impl Point3D {
     }
 
     /// Computes the unsigned distance between this point and another
+    #[inline]
     pub fn dist_from_point(&self, another: &Point3D) -> f64 {
         f64::sqrt(
             (self.x - another.x) * (self.x - another.x)
@@ -41,71 +43,53 @@ impl Point3D {
     }
 }
 
-impl Segment3D {
-    /// Creates a new segment from A to B
-    pub fn new(a: &Point3D, b: &Point3D) -> Self {
-        Segment3D {
-            a: a.clone(),
-            b: b.clone(),
-        }
-    }
-
-    /// Creates a new segment scaled by m and starting from A
-    pub fn get_scaled(&self, m: f64) -> Segment3D {
-        Segment3D {
-            a: self.a.clone(),
-            b: Point3D {
-                x: self.a.x + m * (self.b.x - self.a.x),
-                y: self.a.y + m * (self.b.y - self.a.y),
-                z: self.a.z + m * (self.b.z - self.a.z),
-            },
-        }
-    }
-
-    /// Computes the length of this segment (Euclidean norm)
-    pub fn length(&self) -> f64 {
-        self.a.dist_from_point(&self.b)
-    }
-
-    /// Returns the vector representing this segment, scaled by m
-    pub fn get_vector(&self, m: f64) -> Vector3D {
-        Vector3D {
-            u: m * (self.b.x - self.a.x),
-            v: m * (self.b.y - self.a.y),
-            w: m * (self.b.z - self.a.z),
-        }
-    }
-}
-
 impl Vector3D {
-    /// Creates a new vector scaled by m
-    pub fn new_scaled(u: f64, v: f64, w: f64, m: f64) -> Self {
-        Vector3D {
-            u: m * u,
-            v: m * v,
-            w: m * w,
-        }
-    }
-
-    /// Creates a new vector by adding this vector with another
+    /// Creates a new vector from two points (a to b)
     ///
     /// ```text
-    /// result := α * this + β * another
+    /// vector := b - a
     /// ```
-    pub fn get_added(&self, alpha: f64, beta: f64, another: &Vector3D) -> Vector3D {
+    #[inline]
+    pub fn new_from_points(a: &Point3D, b: &Point3D) -> Self {
         Vector3D {
-            u: alpha * self.u + beta * another.u,
-            v: alpha * self.v + beta * another.v,
-            w: alpha * self.w + beta * another.w,
+            u: b.x - a.x,
+            v: b.y - a.y,
+            w: b.z - a.z,
+        }
+    }
+
+    /// Creates a new scaled vector
+    #[inline]
+    pub fn new_scaled(u: f64, v: f64, w: f64, scale: f64) -> Self {
+        Vector3D {
+            u: scale * u,
+            v: scale * v,
+            w: scale * w,
+        }
+    }
+
+    /// Creates a new vector by subtracting this vector by another
+    ///
+    /// ```text
+    /// result := this - another
+    /// ```
+    #[inline]
+    pub fn get_subtracted(&self, another: &Vector3D) -> Vector3D {
+        Vector3D {
+            u: self.u - another.u,
+            v: self.v - another.v,
+            w: self.w - another.w,
         }
     }
 
     /// Computes the inner product of this vector with another
+    #[inline]
     pub fn dot(&self, another: &Vector3D) -> f64 {
         self.u * another.u + self.v * another.v + self.w * another.w
     }
 
     /// Computes the Euclidean norm of this vector
+    #[inline]
     pub fn norm(&self) -> f64 {
         f64::sqrt(self.u * self.u + self.v * self.v + self.w * self.w)
     }
@@ -121,15 +105,11 @@ mod tests {
 
     #[test]
     fn point3d_get_translated_works() {
-        let a = Point3D {
-            x: -1.0,
-            y: 0.0,
-            z: -2.0,
-        };
+        let a = Point3D { x: 1.0, y: 2.0, z: 3.0 };
         let b = a.get_translated(1.0, 2.0, 3.0);
-        assert_eq!(b.x, 0.0);
-        assert_eq!(b.y, 2.0);
-        assert_eq!(b.z, 1.0);
+        assert_eq!(b.x, 2.0);
+        assert_eq!(b.y, 4.0);
+        assert_eq!(b.z, 6.0);
     }
 
     #[test]
@@ -150,49 +130,13 @@ mod tests {
     }
 
     #[test]
-    fn segment3d_new_works() {
-        let a = Point3D { x: 0.0, y: 2.0, z: 3.0 };
-        let b = Point3D { x: 3.0, y: 2.0, z: 1.0 };
-        let segment = Segment3D::new(&a, &b);
-        assert_eq!(segment.a.x, 0.0);
-        assert_eq!(segment.a.y, 2.0);
-        assert_eq!(segment.a.z, 3.0);
-        assert_eq!(segment.b.x, 3.0);
-        assert_eq!(segment.b.y, 2.0);
-        assert_eq!(segment.b.z, 1.0);
-    }
-
-    #[test]
-    fn segment3d_get_scaled_works() {
-        let a = Point3D { x: 0.0, y: 2.0, z: 3.0 };
-        let b = Point3D { x: 3.0, y: 2.0, z: 1.0 };
-        let segment = Segment3D::new(&a, &b);
-        let scaled = segment.get_scaled(0.5);
-        assert_eq!(scaled.a.x, 0.0);
-        assert_eq!(scaled.a.y, 2.0);
-        assert_eq!(scaled.a.z, 3.0);
-        assert_eq!(scaled.b.x, 1.5);
-        assert_eq!(scaled.b.y, 2.0);
-        assert_eq!(scaled.b.z, 2.0);
-    }
-
-    #[test]
-    fn segment3d_length_works() {
-        let a = Point3D { x: 0.0, y: 2.0, z: 3.0 };
-        let b = Point3D { x: 3.0, y: 2.0, z: 1.0 };
-        let segment = Segment3D::new(&a, &b);
-        assert_approx_eq!(segment.length(), f64::sqrt(13.0), 1e-15)
-    }
-
-    #[test]
-    fn segment3d_get_vector_works() {
-        let a = Point3D { x: 0.0, y: 2.0, z: 3.0 };
-        let b = Point3D { x: 3.0, y: 2.0, z: 1.0 };
-        let segment = Segment3D::new(&a, &b);
-        let vector = segment.get_vector(0.5);
-        assert_eq!(vector.u, 1.5);
-        assert_eq!(vector.v, 0.0);
-        assert_eq!(vector.w, -1.0);
+    fn vector3d_new_from_points_works() {
+        let a = Point3D { x: 1.0, y: 2.0, z: 3.0 };
+        let b = Point3D { x: 4.0, y: 5.0, z: 6.0 };
+        let vector = Vector3D::new_from_points(&a, &b);
+        assert_eq!(vector.u, 3.0);
+        assert_eq!(vector.v, 3.0);
+        assert_eq!(vector.w, 3.0);
     }
 
     #[test]
@@ -204,13 +148,13 @@ mod tests {
     }
 
     #[test]
-    fn vector3d_get_added_works() {
+    fn vector3d_get_subtracted_works() {
         let x = Vector3D { u: 1.0, v: 2.0, w: 3.0 };
         let y = Vector3D { u: 3.0, v: 2.0, w: 1.0 };
-        let z = x.get_added(0.5, 3.0, &y);
-        assert_eq!(z.u, 9.5);
-        assert_eq!(z.v, 7.0);
-        assert_eq!(z.w, 4.5);
+        let z = x.get_subtracted(&y);
+        assert_eq!(z.u, -2.0);
+        assert_eq!(z.v, 0.0);
+        assert_eq!(z.w, 2.0);
     }
 
     #[test]
