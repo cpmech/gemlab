9e46270969f0116ec9d52d32991d4dabab1740da
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-11-07 10:55:15 +1000

    Remove geometry definitions. Use arrays in algorithms

--- src/geometry/algorithms.rs
+++ src/geometry/algorithms.rs
@@ -1,7 +1,6 @@
-use super::{Point2D, Point3D, Vector2D, Vector3D};
 use crate::StrError;
 
-/// Computes the distance between a point and a segment in 2D
+/// Computes the distance between a point and a segment
 ///
 /// ```text
 ///                 c
@@ -26,55 +25,47 @@ use crate::StrError;
 /// q := x - p
 /// distance := norm(q)
 /// ```
-pub fn point_segment_distance_2d(a: &Point2D, b: &Point2D, c: &Point2D) -> Result<f64, StrError> {
-    let n = Vector2D::new_from_points(a, b); // b - a
-    let denominator = n.dot(&n); // n ⋅ n
-    if denominator <= f64::EPSILON {
-        return Err("segment is too short");
+pub fn point_segment_distance(a: &[f64], b: &[f64], c: &[f64]) -> Result<f64, StrError> {
+    let ndim = a.len();
+    if ndim < 2 || ndim > 3 {
+        return Err("a.len() == ndim must be 2 or 3");
     }
-    let x = Vector2D::new_from_points(a, c); // c - a
-    let scale = x.dot(&n) / denominator; // (x ⋅ n) / (n ⋅ n)
-    let p = Vector2D::new_scaled(n.u, n.v, scale);
-    let q = x.get_subtracted(&p); // x - p
-    Ok(q.norm())
-}
-
-/// Computes the distance between a point and a segment in 3D
-///
-/// ```text
-///                 c
-///                /^
-/// ⇀             / | ⇀   ⇀   ⇀
-/// x = vec(c-a) /  | q = x - p
-///             /   |       
-///            /    |
-///           a----->-------------b
-///             ⇀              ⇀
-///             p = projection(x)
-///
-///           a-------------------b
-///               ⇀
-///               n = vec(b-c)
-/// ```
-///
-/// ```text
-/// x := c - a
-/// n := b - a
-/// p := ((x ⋅ n) / (n ⋅ n)) * n
-/// q := x - p
-/// distance := norm(q)
-/// ```
-pub fn point_segment_distance_3d(a: &Point3D, b: &Point3D, c: &Point3D) -> Result<f64, StrError> {
-    let n = Vector3D::new_from_points(a, b); // b - a
-    let denominator = n.dot(&n); // n ⋅ n
-    if denominator <= f64::EPSILON {
-        return Err("segment is too short");
+    if b.len() != ndim {
+        return Err("b.len() must equal a.len() == ndim");
     }
-    let x = Vector3D::new_from_points(a, c); // c - a
-    let scale = x.dot(&n) / denominator; // (x ⋅ n) / (n ⋅ n)
-    let p = Vector3D::new_scaled(n.u, n.v, n.w, scale);
-    let q = x.get_subtracted(&p); // x - p
-    Ok(q.norm())
+    if c.len() != ndim {
+        return Err("c.len() must equal a.len() == ndim");
+    }
+    let distance: f64;
+    if ndim == 2 {
+        // n ⋅ n  with n = b - a
+        let n_dot_n = (b[0] - a[0]) + (b[1] - a[1]);
+        if n_dot_n <= f64::EPSILON {
+            return Err("segment is too short");
+        }
+        // x ⋅ n  with x = c - a
+        let x_dot_n = (c[0] - a[0]) * (b[0] - a[0]) + (c[1] - a[1]) * (b[1] - a[1]);
+        // q = x - p = (c-a) - (b-a) * (x⋅n)/(n⋅n)
+        let q0 = (c[0] - a[0]) - (b[0] - a[0]) * x_dot_n / n_dot_n;
+        let q1 = (c[1] - a[1]) - (b[1] - a[1]) * x_dot_n / n_dot_n;
+        // norm(q)
+        distance = f64::sqrt(q0 * q0 + q1 * q1);
+    } else {
+        // n ⋅ n  with n = b - a
+        let n_dot_n = (b[0] - a[0]) + (b[1] - a[1]) + (b[2] - a[2]);
+        if n_dot_n <= f64::EPSILON {
+            return Err("segment is too short");
+        }
+        // x ⋅ n  with x = c - a
+        let x_dot_n = (c[0] - a[0]) * (b[0] - a[0]) + (c[1] - a[1]) * (b[1] - a[1]) + (c[2] - a[2]) * (b[2] - a[2]);
+        // q = x - p = (c-a) - (b-a) * (x⋅n)/(n⋅n)
+        let q0 = (c[0] - a[0]) - (b[0] - a[0]) * x_dot_n / n_dot_n;
+        let q1 = (c[1] - a[1]) - (b[1] - a[1]) * x_dot_n / n_dot_n;
+        let q2 = (c[2] - a[2]) - (b[2] - a[2]) * x_dot_n / n_dot_n;
+        // norm(q)
+        distance = f64::sqrt(q0 * q0 + q1 * q1 + q2 * q2);
+    }
+    Ok(distance)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -86,118 +77,122 @@ mod tests {
     use russell_chk::assert_approx_eq;
 
     #[test]
-    fn point_segment_distance_2d_fails_on_short_segment() -> Result<(), StrError> {
-        let a = Point2D { x: 0.0, y: 0.0 };
-        let b = Point2D {
-            x: f64::EPSILON,
-            y: f64::EPSILON,
-        };
-        let c = Point2D { x: 1.0, y: 1.0 };
+    fn point_segment_distance_fails_on_wrong_input() -> Result<(), StrError> {
         assert_eq!(
-            point_segment_distance_2d(&a, &b, &c).err(),
-            Some("segment is too short")
+            point_segment_distance(&[0.0], &[1.0, 1.0], &[2.0, 2.0]).err(),
+            Some("a.len() == ndim must be 2 or 3")
+        );
+        assert_eq!(
+            point_segment_distance(&[0.0, 0.0], &[1.0], &[2.0, 2.0]).err(),
+            Some("b.len() must equal a.len() == ndim")
+        );
+        assert_eq!(
+            point_segment_distance(&[0.0, 0.0], &[1.0, 1.0], &[2.0]).err(),
+            Some("c.len() must equal a.len() == ndim")
         );
         Ok(())
     }
 
     #[test]
-    fn point_segment_distance_2d_works() -> Result<(), StrError> {
-        let a = Point2D { x: 0.0, y: 0.0 };
-        let b = Point2D { x: 1.0, y: 1.0 };
-        let c = a.clone();
-        let distance = point_segment_distance_2d(&a, &b, &c)?;
-        assert_eq!(distance, 0.0);
-
-        let c = b.clone();
-        let distance = point_segment_distance_2d(&a, &b, &c)?;
-        assert_eq!(distance, 0.0);
-
-        let c = Point2D { x: 1.0, y: 0.0 };
-        let distance = point_segment_distance_2d(&a, &b, &c)?;
-        assert_approx_eq!(distance, SQRT_2 / 2.0, 1e-15);
-
-        let c = Point2D { x: 0.0, y: 1.0 };
-        let distance = point_segment_distance_2d(&a, &b, &c)?;
-        assert_approx_eq!(distance, SQRT_2 / 2.0, 1e-15);
-
-        let c = Point2D { x: 0.5, y: 0.5 };
-        let distance = point_segment_distance_2d(&a, &b, &c)?;
-        assert_eq!(distance, 0.0);
-
-        let a = Point2D { x: 0.0, y: 0.0 };
-        let b = Point2D { x: 1.0, y: 0.0 };
-        let c = Point2D { x: 1.0, y: 0.5 };
-        let distance = point_segment_distance_2d(&a, &b, &c)?;
-        assert_eq!(distance, 0.5);
-
-        let a = Point2D { x: 0.0, y: 0.0 };
-        let b = Point2D { x: 0.0, y: 1.0 };
-        let c = Point2D { x: 0.5, y: 1.0 };
-        let distance = point_segment_distance_2d(&a, &b, &c)?;
-        assert_eq!(distance, 0.5);
+    fn point_segment_distance_2d_fails_on_short_segment() -> Result<(), StrError> {
+        let a = &[0.0, 0.0];
+        let b = &[0.0, f64::EPSILON];
+        let c = &[1.0, 1.0];
+        assert_eq!(point_segment_distance(a, b, c).err(), Some("segment is too short"));
         Ok(())
     }
 
     #[test]
     fn point_segment_distance_3d_fails_on_short_segment() -> Result<(), StrError> {
-        let a = Point3D { x: 0.0, y: 0.0, z: 0.0 };
-        let b = Point3D {
-            x: f64::EPSILON,
-            y: f64::EPSILON,
-            z: f64::EPSILON,
-        };
-        let c = Point3D { x: 1.0, y: 1.0, z: 1.0 };
-        assert_eq!(
-            point_segment_distance_3d(&a, &b, &c).err(),
-            Some("segment is too short")
-        );
+        let a = &[0.0, 0.0, 0.0];
+        let b = &[0.0, 0.0, f64::EPSILON];
+        let c = &[1.0, 1.0, 1.0];
+        assert_eq!(point_segment_distance(a, b, c).err(), Some("segment is too short"));
+        Ok(())
+    }
+
+    #[test]
+    fn point_segment_distance_2d_works() -> Result<(), StrError> {
+        let a = &[0.0, 0.0];
+        let b = &[1.0, 1.0];
+        let c = &a.clone();
+        let distance = point_segment_distance(a, b, c)?;
+        assert_eq!(distance, 0.0);
+
+        let c = &b.clone();
+        let distance = point_segment_distance(a, b, c)?;
+        assert_eq!(distance, 0.0);
+
+        let c = &[1.0, 0.0];
+        let distance = point_segment_distance(a, b, c)?;
+        assert_approx_eq!(distance, SQRT_2 / 2.0, 1e-15);
+
+        let c = &[0.0, 1.0];
+        let distance = point_segment_distance(a, b, c)?;
+        assert_approx_eq!(distance, SQRT_2 / 2.0, 1e-15);
+
+        let c = &[0.5, 0.5];
+        let distance = point_segment_distance(a, b, c)?;
+        assert_eq!(distance, 0.0);
+
+        let a = &[0.0, 0.0];
+        let b = &[1.0, 0.0];
+        let c = &[1.0, 0.5];
+        let distance = point_segment_distance(a, b, c)?;
+        assert_eq!(distance, 0.5);
+
+        let a = &[0.0, 0.0];
+        let b = &[0.0, 1.0];
+        let c = &[0.5, 1.0];
+        let distance = point_segment_distance(a, b, c)?;
+        assert_eq!(distance, 0.5);
         Ok(())
     }
 
     #[test]
     fn point_segment_distance_3d_works() -> Result<(), StrError> {
-        let a = Point3D { x: 0.0, y: 0.0, z: 0.0 };
-        let b = Point3D { x: 1.0, y: 1.0, z: 1.0 };
-        let c = a.clone();
-        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        let a = &[0.0, 0.0, 0.0];
+        let b = &[1.0, 1.0, 1.0];
+        let c = &a.clone();
+        let distance = point_segment_distance(a, b, c)?;
         assert_eq!(distance, 0.0);
 
-        let c = b.clone();
-        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        let c = &b.clone();
+        let distance = point_segment_distance(a, b, c)?;
         assert_eq!(distance, 0.0);
 
-        let c = Point3D { x: 1.0, y: 1.0, z: 0.0 };
-        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        let c = &[1.0, 1.0, 0.0];
+        let distance = point_segment_distance(a, b, c)?;
         assert_approx_eq!(distance, SQRT_2_BY_3, 1e-15);
 
-        let c = Point3D { x: 0.0, y: 1.0, z: 1.0 };
-        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        let c = &[0.0, 1.0, 1.0];
+        let distance = point_segment_distance(a, b, c)?;
         assert_approx_eq!(distance, SQRT_2_BY_3, 1e-15);
 
-        let c = Point3D { x: 1.0, y: 0.0, z: 1.0 };
-        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        let c = &[1.0, 0.0, 1.0];
+        let distance = point_segment_distance(a, b, c)?;
         assert_approx_eq!(distance, SQRT_2_BY_3, 1e-15);
 
-        let c = Point3D { x: 0.5, y: 0.5, z: 0.5 };
-        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        let c = &[0.5, 0.5, 0.5];
+        let distance = point_segment_distance(a, b, c)?;
         assert_eq!(distance, 0.0);
 
-        let a = Point3D { x: 0.0, y: 0.0, z: 0.0 };
-        let b = Point3D { x: 1.0, y: 0.0, z: 0.0 };
-        let c = Point3D { x: 1.0, y: 0.5, z: 0.0 };
-        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        let a = &[0.0, 0.0, 0.0];
+        let b = &[1.0, 0.0, 0.0];
+        let c = &[1.0, 0.5, 0.0];
+        let distance = point_segment_distance(a, b, c)?;
         assert_eq!(distance, 0.5);
 
-        let a = Point3D { x: 0.0, y: 0.0, z: 0.0 };
-        let b = Point3D { x: 0.0, y: 1.0, z: 0.0 };
-        let c = Point3D { x: 0.5, y: 1.0, z: 0.0 };
-        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        let a = &[0.0, 0.0, 0.0];
+        let b = &[0.0, 1.0, 0.0];
+        let c = &[0.5, 1.0, 0.0];
+        let distance = point_segment_distance(a, b, c)?;
         assert_eq!(distance, 0.5);
 
-        let a = Point3D { x: 0.0, y: 0.0, z: 0.0 };
-        let b = Point3D { x: 0.0, y: 0.0, z: 1.0 };
-        let c = Point3D { x: 0.0, y: 0.5, z: 1.0 };
-        let distance = point_segment_distance_3d(&a, &b, &c)?;
+        let a = &[0.0, 0.0, 0.0];
+        let b = &[0.0, 0.0, 1.0];
+        let c = &[0.0, 0.5, 1.0];
+        let distance = point_segment_distance(a, b, c)?;
         assert_eq!(distance, 0.5);
         Ok(())
     }
