95e7c45092c290db4a1771ec6b04166d9f8799c7
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-11-28 18:42:03 +1000

    [wip] Impl select_int_points method

--- src/shapes/integration.rs
+++ src/shapes/integration.rs
@@ -1,9 +1,66 @@
 use super::*;
 use crate::StrError;
 
+/// Defines options to select integration points
+#[derive(Eq, PartialEq)]
+pub enum OptionIntPoint {
+    /// Option for Tri (points on edge)
+    Edge,
+
+    /// Option for Qua and Hex (Wilson formula)
+    WilsonStable,
+}
+
 impl Shape {
-    /// Selects a set of integrations points
-    pub fn select_int_points(&mut self, nip: usize) -> Result<(), StrError> {
+    /// Selects integrations points and weights
+    ///
+    /// # Options
+    ///
+    /// ## nip for Lin class
+    ///
+    /// * `1` -- Legendre points
+    /// * `2` -- Legendre points
+    /// * `3` -- Legendre points
+    /// * `4` -- Legendre points
+    /// * `5` -- Legendre points
+    ///
+    /// ## nip for Tri class
+    ///
+    /// * `1` -- Internal points
+    /// * `3`:
+    ///     - `Edge` -- Points on edge
+    ///     - Otherwise, internal points
+    /// * `4` -- Internal points
+    /// * `12` -- Internal points
+    /// * `16` -- Internal points
+    ///
+    /// ## nip for Qua class
+    ///
+    /// * `1` -- Legendre points
+    /// * `4` -- Legendre points
+    /// * `5`:
+    ///     - `WilsonStable` -- Wilson's "Stable" version with w0=0.004 and wa=0.999 to mimic 4-point rule
+    ///     - Otherwise, Wilson's formula
+    /// * `9` -- Legendre points
+    /// * `16` -- Legendre points
+    ///
+    /// ## nip for Tet class
+    ///
+    /// * `1` -- Internal points
+    /// * `4` -- Internal points
+    /// * `5` -- Internal points
+    /// * `6` -- Internal points
+    ///
+    /// ## nip for Hex class
+    ///
+    /// * `6` -- Iron's formula
+    /// * `8` -- Legendre points
+    /// * `9`:
+    ///     - `WilsonStable` -- Wilson's "Stable" version
+    ///     - Otherwise, Wilson's formula
+    /// * `14` -- Iron's formula
+    /// * `27` -- Legendre points
+    pub fn select_int_points(&mut self, nip: usize, option: OptionIntPoint) -> Result<(), StrError> {
         match self.class {
             GeoClass::Lin => match nip {
                 1 => self.ip_data = &IP_LIN_LEGENDRE_1,
@@ -11,34 +68,57 @@ impl Shape {
                 3 => self.ip_data = &IP_LIN_LEGENDRE_3,
                 4 => self.ip_data = &IP_LIN_LEGENDRE_4,
                 5 => self.ip_data = &IP_LIN_LEGENDRE_5,
-                _ => return Err("integration points set is not available for this GeoClass"),
+                _ => return Err("number of integration points is not available for Lin class"),
             },
             GeoClass::Tri => match nip {
                 1 => self.ip_data = &IP_TRI_INTERNAL_1,
-                3 => self.ip_data = &IP_TRI_INTERNAL_3,
-                _ => return Err("integration points set is not available for this GeoClass"),
+                3 => {
+                    if option == OptionIntPoint::Edge {
+                        self.ip_data = &IP_TRI_EDGE_3
+                    } else {
+                        self.ip_data = &IP_TRI_INTERNAL_3
+                    }
+                }
+                4 => self.ip_data = &IP_TRI_INTERNAL_4,
+                12 => self.ip_data = &IP_TRI_INTERNAL_12,
+                16 => self.ip_data = &IP_TRI_INTERNAL_16,
+                _ => return Err("number of integration points is not available for Tri class"),
             },
             GeoClass::Qua => match nip {
                 1 => self.ip_data = &IP_QUA_LEGENDRE_1,
                 4 => self.ip_data = &IP_QUA_LEGENDRE_4,
+                5 => {
+                    if option == OptionIntPoint::WilsonStable {
+                        self.ip_data = &IP_QUA_WILSON_STABLE_5
+                    } else {
+                        self.ip_data = &IP_QUA_WILSON_CORNER_5
+                    }
+                }
+                8 => self.ip_data = &IP_QUA_WILSON_8,
                 9 => self.ip_data = &IP_QUA_LEGENDRE_9,
                 16 => self.ip_data = &IP_QUA_LEGENDRE_16,
-                _ => return Err("integration points set is not available for this GeoClass"),
+                _ => return Err("number of integration points is not available for Qua class"),
             },
             GeoClass::Tet => match nip {
                 1 => self.ip_data = &IP_TET_INTERNAL_1,
                 4 => self.ip_data = &IP_TET_INTERNAL_4,
                 5 => self.ip_data = &IP_TET_INTERNAL_5,
                 6 => self.ip_data = &IP_TET_INTERNAL_6,
-                _ => return Err("integration points set is not available for this GeoClass"),
+                _ => return Err("number of integration points is not available for Tet class"),
             },
             GeoClass::Hex => match nip {
-                8 => self.ip_data = &IP_HEX_LEGENDRE_8,
-                9 => self.ip_data = &IP_HEX_WILSON_CORNER_9,
                 6 => self.ip_data = &IP_HEX_IRONS_6,
+                8 => self.ip_data = &IP_HEX_LEGENDRE_8,
+                9 => {
+                    if option == OptionIntPoint::WilsonStable {
+                        self.ip_data = &IP_HEX_WILSON_STABLE_9
+                    } else {
+                        self.ip_data = &IP_HEX_WILSON_CORNER_9
+                    }
+                }
                 14 => self.ip_data = &IP_HEX_IRONS_14,
                 27 => self.ip_data = &IP_HEX_LEGENDRE_27,
-                _ => return Err("integration points set is not available for this GeoClass"),
+                _ => return Err("number of integration points is not available for Hex class"),
             },
         }
         Ok(())
