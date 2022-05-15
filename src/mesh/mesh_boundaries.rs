bbd722ecf51a36bd96b10dfd615dc5e0e33e2865
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-05-16 00:39:25 +1000

    Impl map_faces and add tests

--- src/mesh/mesh_boundaries.rs
+++ src/mesh/mesh_boundaries.rs
@@ -1,6 +1,6 @@
 use super::{alloc_cell_shapes, Cell, CellId, Mesh, PointId};
 use crate::{shapes::Shape, StrError};
-use russell_lab::sort2;
+use russell_lab::{sort2, sort4};
 use std::collections::{HashMap, HashSet};
 
 /// Aliases (usize,usize) as the key of Edge
@@ -80,10 +80,16 @@ impl MeshBoundaries {
 
 /// Maps all edges (internal and boundary) in 2D
 ///
+/// # Output
+///
 /// Returns a map relating edge keys to `Vec<(cell_id, e)>` where:
 ///
 /// * `cell_id` -- the id of the cell sharing the edge
 /// * `e` -- is the cell's local edge index
+///
+/// # Panics
+///
+/// The `mesh.space_ndim` must be equal to 2, otherwise a panic occurs.
 pub fn map_edges_2d(mesh: &Mesh, shapes: &Vec<Shape>) -> HashMap<EdgeKey, Vec<(CellId, usize)>> {
     assert_eq!(mesh.space_ndim, 2);
     let mut edges: HashMap<EdgeKey, Vec<(CellId, usize)>> = HashMap::new();
@@ -101,6 +107,46 @@ pub fn map_edges_2d(mesh: &Mesh, shapes: &Vec<Shape>) -> HashMap<EdgeKey, Vec<(C
         }
     });
     edges
+}
+
+/// Maps all faces (internal and boundary)
+///
+/// Returns a map relating face keys to `Vec<(cell_id, f)>` where:
+///
+/// * `cell_id` -- the id of the cell sharing the face
+/// * `f` -- is the cell's local face index
+///
+/// # Panics
+///
+/// The `mesh.space_ndim` must be equal to 3, otherwise a panic occurs.
+pub fn map_faces(mesh: &Mesh, shapes: &Vec<Shape>) -> HashMap<FaceKey, Vec<(CellId, usize)>> {
+    assert_eq!(mesh.space_ndim, 3);
+    let mut faces: HashMap<FaceKey, Vec<(CellId, usize)>> = HashMap::new();
+    mesh.cells.iter().zip(shapes).for_each(|(cell, shape)| {
+        if shape.geo_ndim == 3 {
+            for f in 0..shape.nface {
+                let mut face_key: FaceKey = if shape.face_nnode > 3 {
+                    (
+                        cell.points[shape.face_node_id(f, 0)],
+                        cell.points[shape.face_node_id(f, 1)],
+                        cell.points[shape.face_node_id(f, 2)],
+                        cell.points[shape.face_node_id(f, 3)],
+                    )
+                } else {
+                    (
+                        cell.points[shape.face_node_id(f, 0)],
+                        cell.points[shape.face_node_id(f, 1)],
+                        cell.points[shape.face_node_id(f, 2)],
+                        mesh.points.len(),
+                    )
+                };
+                sort4(&mut face_key);
+                let data = faces.entry(face_key).or_insert(Vec::new());
+                data.push((cell.id, f));
+            }
+        }
+    });
+    faces
 }
 
 /*
@@ -166,7 +212,7 @@ fn find_boundaries_2d(
 
 #[cfg(test)]
 mod tests {
-    use super::map_edges_2d;
+    use super::{map_edges_2d, map_faces};
     use crate::mesh::{alloc_cell_shapes, Samples};
     use crate::StrError;
 
@@ -190,6 +236,124 @@ mod tests {
         assert_eq!(edges.get(&(2, 3)).unwrap(), &[(0, 2)]);
         assert_eq!(edges.get(&(2, 5)).unwrap(), &[(1, 2)]);
         assert_eq!(edges.get(&(4, 5)).unwrap(), &[(1, 1)]);
+
+        //           4---------3
+        //           |         |
+        //           |   [1]   |
+        //           |         |
+        //  0--------1---------2
+        let mesh = Samples::mixed_shapes_2d();
+        let shapes = alloc_cell_shapes(&mesh)?;
+        let edges = map_edges_2d(&mesh, &shapes);
+        let mut keys: Vec<_> = edges.keys().collect();
+        keys.sort();
+        assert_eq!(keys, [&(1, 2), &(1, 4), &(2, 3), &(3, 4)]);
+        assert_eq!(edges.get(&(1, 2)).unwrap(), &[(1, 0)]);
+        assert_eq!(edges.get(&(1, 4)).unwrap(), &[(1, 3)]);
+        assert_eq!(edges.get(&(2, 3)).unwrap(), &[(1, 1)]);
+        assert_eq!(edges.get(&(3, 4)).unwrap(), &[(1, 2)]);
+        Ok(())
+    }
+
+    #[test]
+    fn map_faces_works() -> Result<(), StrError> {
+        //      8-------------11
+        //     /.             /|
+        //    / .            / |
+        //   /  .           /  |
+        //  /   .          /   |       id = 1
+        // 9-------------10    |       attribute_id = 2
+        // |    .         |    |
+        // |    4---------|----7
+        // |   /.         |   /|
+        // |  / .         |  / |
+        // | /  .         | /  |
+        // |/   .         |/   |
+        // 5--------------6    |       id = 0
+        // |    .         |    |       attribute_id = 1
+        // |    0---------|----3
+        // |   /          |   /
+        // |  /           |  /
+        // | /            | /
+        // |/             |/
+        // 1--------------2
+        let mesh = Samples::two_cubes_vertical();
+        let shapes = alloc_cell_shapes(&mesh)?;
+        let faces = map_faces(&mesh, &shapes);
+        let mut keys: Vec<_> = faces.keys().collect();
+        keys.sort();
+        assert_eq!(
+            keys,
+            [
+                &(0, 1, 2, 3),
+                &(0, 1, 4, 5),
+                &(0, 3, 4, 7),
+                &(1, 2, 5, 6),
+                &(2, 3, 6, 7),
+                &(4, 5, 6, 7),
+                &(4, 5, 8, 9),
+                &(4, 7, 8, 11),
+                &(5, 6, 9, 10),
+                &(6, 7, 10, 11),
+                &(8, 9, 10, 11),
+            ]
+        );
+        assert_eq!(faces.get(&(0, 1, 2, 3)).unwrap(), &[(0, 4)]);
+        assert_eq!(faces.get(&(0, 1, 4, 5)).unwrap(), &[(0, 2)]);
+        assert_eq!(faces.get(&(0, 3, 4, 7)).unwrap(), &[(0, 0)]);
+        assert_eq!(faces.get(&(1, 2, 5, 6)).unwrap(), &[(0, 1)]);
+        assert_eq!(faces.get(&(2, 3, 6, 7)).unwrap(), &[(0, 3)]);
+        assert_eq!(faces.get(&(4, 5, 6, 7)).unwrap(), &[(0, 5), (1, 4)]);
+        assert_eq!(faces.get(&(4, 5, 8, 9)).unwrap(), &[(1, 2)]);
+        assert_eq!(faces.get(&(4, 7, 8, 11)).unwrap(), &[(1, 0)]);
+        assert_eq!(faces.get(&(5, 6, 9, 10)).unwrap(), &[(1, 1)]);
+        assert_eq!(faces.get(&(6, 7, 10, 11)).unwrap(), &[(1, 3)]);
+        assert_eq!(faces.get(&(8, 9, 10, 11)).unwrap(), &[(1, 5)]);
+
+        //                       4------------7-----------10
+        //                      /.           /|            |
+        //                     / .          / |            |
+        //                    /  .         /  |            |
+        //                   /   .        /   |            |
+        //                  5------------6    |            |
+        //                  |    .       |`.  |            |
+        //                  |    0-------|--`.3------------9
+        //                  |   /        |   /`.          /
+        //                  |  /         |  /   `.       /
+        //                  | /          | /      `.    /
+        //                  |/           |/         `. /
+        //  12-----11-------1------------2------------8
+        //
+        let mesh = Samples::mixed_shapes_3d();
+        let shapes = alloc_cell_shapes(&mesh)?;
+        let faces = map_faces(&mesh, &shapes);
+        let mut keys: Vec<_> = faces.keys().collect();
+        keys.sort();
+        assert_eq!(
+            keys,
+            [
+                &(0, 1, 2, 3),
+                &(0, 1, 4, 5),
+                &(0, 3, 4, 7),
+                &(1, 2, 5, 6),
+                &(2, 3, 6, 7),
+                &(2, 3, 6, 13),
+                &(2, 3, 8, 13),
+                &(2, 6, 8, 13),
+                &(3, 6, 8, 13),
+                &(4, 5, 6, 7),
+            ]
+        );
+        assert_eq!(faces.get(&(0, 1, 2, 3)).unwrap(), &[(0, 4)]);
+        assert_eq!(faces.get(&(0, 1, 4, 5)).unwrap(), &[(0, 2)]);
+        assert_eq!(faces.get(&(0, 3, 4, 7)).unwrap(), &[(0, 0)]);
+        assert_eq!(faces.get(&(1, 2, 5, 6)).unwrap(), &[(0, 1)]);
+        assert_eq!(faces.get(&(2, 3, 6, 7)).unwrap(), &[(0, 3)]);
+        assert_eq!(faces.get(&(2, 3, 6, 13)).unwrap(), &[(1, 0)]);
+        assert_eq!(faces.get(&(2, 3, 8, 13)).unwrap(), &[(1, 2)]);
+        assert_eq!(faces.get(&(2, 6, 8, 13)).unwrap(), &[(1, 1)]);
+        assert_eq!(faces.get(&(3, 6, 8, 13)).unwrap(), &[(1, 3)]);
+        assert_eq!(faces.get(&(4, 5, 6, 7)).unwrap(), &[(0, 5)]);
         Ok(())
     }
 }
