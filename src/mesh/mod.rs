9f72bba62c88ee9858ea09319989f53410f9131a
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-05-16 15:34:40 +1000

    Rename boundary file

--- src/mesh/mod.rs
+++ src/mesh/mod.rs
@@ -1,17 +1,17 @@
 //! Defines finite element mesh and generators
 
 mod block;
+mod boundary;
 mod edges_and_faces;
 mod enums;
 mod mesh;
-mod mesh_boundaries;
 mod mesh_shapes;
 mod read_text_mesh;
 mod samples;
 pub use crate::mesh::block::*;
+pub use crate::mesh::boundary::*;
 pub use crate::mesh::edges_and_faces::*;
 pub use crate::mesh::enums::*;
 pub use crate::mesh::mesh::*;
-pub use crate::mesh::mesh_boundaries::*;
 pub use crate::mesh::mesh_shapes::*;
 pub use crate::mesh::samples::*;
