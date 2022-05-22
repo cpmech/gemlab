5d722d663a6f8be2654241d33db4d4da699e7d25
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-05-22 22:55:55 +1000

    Add tests

--- src/mesh/normal_vector.rs
+++ src/mesh/normal_vector.rs
@@ -157,7 +157,245 @@ mod tests {
                 assert_vec_approx_eq!(n.value.as_data(), solution, 1e-15);
             }
         }
+        Ok(())
+    }
 
+    #[test]
+    fn at_edge_and_evaluate_work_qua8() -> Result<(), StrError> {
+        // 14------16------13------20------18
+        //  |               |               |
+        //  |               |               |
+        // 17              15              19
+        //  |               |               |
+        //  |               |               |
+        //  3-------6-------2------12-------9
+        //  |               |               |
+        //  |               |               |
+        //  7               5              11
+        //  |               |               |
+        //  |               |               |
+        //  0-------4-------1------10-------8
+        let mesh = Samples::block_2d_four_qua8();
+        let shapes = Shapes::new(&mesh)?;
+        let boundary = Boundary::new(&mesh, &shapes)?;
+
+        // the magnitude (l) of the normal vector should be equal to
+        // 0.5 = edge_length / 2.0 where 2.0 corresponds to the edge_length in the reference system
+        let l = 0.5; // magnitude of normal vector
+
+        // edge keys and correct normal vectors (solutions)
+        let edge_keys_and_solutions = [
+            // bottom
+            (vec![(0, 1), (1, 8)], [0.0, -l]),
+            // right
+            (vec![(8, 9), (9, 18)], [l, 0.0]),
+            // top
+            (vec![(13, 14), (13, 18)], [0.0, l]),
+            // left
+            (vec![(0, 3), (3, 14)], [-l, 0.0]),
+        ];
+
+        // check if the normal vectors at boundary are outward
+        let ksi = &[0.0, 0.0];
+        for (edge_keys, solution) in &edge_keys_and_solutions {
+            for edge_key in edge_keys {
+                let mut n = NormalVector::at_edge(&mesh, &boundary, *edge_key)?;
+                n.evaluate(ksi)?;
+                assert_vec_approx_eq!(n.value.as_data(), solution, 1e-15);
+            }
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn at_edge_and_evaluate_work_qua9() -> Result<(), StrError> {
+        // 16------18------15------23------21
+        //  |               |               |
+        //  |               |               |
+        // 19      20      17      24      22
+        //  |               |               |
+        //  |               |               |
+        //  3-------6-------2------13------10
+        //  |               |               |
+        //  |               |               |
+        //  7       8       5      14      12
+        //  |               |               |
+        //  |               |               |
+        //  0-------4-------1------11-------9
+        let mesh = Samples::block_2d_four_qua9();
+        let shapes = Shapes::new(&mesh)?;
+        let boundary = Boundary::new(&mesh, &shapes)?;
+
+        // the magnitude (l) of the normal vector should be equal to
+        // 0.5 = edge_length / 2.0 where 2.0 corresponds to the edge_length in the reference system
+        let l = 0.5; // magnitude of normal vector
+
+        // edge keys and correct normal vectors (solutions)
+        let edge_keys_and_solutions = [
+            // bottom
+            (vec![(0, 1), (1, 9)], [0.0, -l]),
+            // right
+            (vec![(9, 10), (10, 21)], [l, 0.0]),
+            // top
+            (vec![(15, 16), (15, 21)], [0.0, l]),
+            // left
+            (vec![(0, 3), (3, 16)], [-l, 0.0]),
+        ];
+
+        // check if the normal vectors at boundary are outward
+        let ksi = &[0.0, 0.0];
+        for (edge_keys, solution) in &edge_keys_and_solutions {
+            for edge_key in edge_keys {
+                let mut n = NormalVector::at_edge(&mesh, &boundary, *edge_key)?;
+                n.evaluate(ksi)?;
+                assert_vec_approx_eq!(n.value.as_data(), solution, 1e-14);
+            }
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn at_edge_and_evaluate_work_qua12() -> Result<(), StrError> {
+        // 21---26---23----20---32---30----28
+        //  |               |               |
+        // 24              25              31
+        //  |               |               |
+        // 27              22              29
+        //  |               |               |
+        //  3---10-----6----2---19---16----13
+        //  |               |               |
+        //  7               9              18
+        //  |               |               |
+        // 11               5              15
+        //  |               |               |
+        //  0----4-----8----1---14---17----12
+        let mesh = Samples::block_2d_four_qua12();
+        let shapes = Shapes::new(&mesh)?;
+        let boundary = Boundary::new(&mesh, &shapes)?;
+
+        // the magnitude (l) of the normal vector should be equal to
+        // 0.75 = edge_length / 2.0 where 2.0 corresponds to the edge_length in the reference system
+        let l = 0.75; // magnitude of normal vector
+
+        // edge keys and correct normal vectors (solutions)
+        let edge_keys_and_solutions = [
+            // bottom
+            (vec![(0, 1), (1, 12)], [0.0, -l]),
+            // right
+            (vec![(12, 13), (13, 28)], [l, 0.0]),
+            // top
+            (vec![(20, 21), (20, 28)], [0.0, l]),
+            // left
+            (vec![(0, 3), (3, 21)], [-l, 0.0]),
+        ];
+
+        // check if the normal vectors at boundary are outward
+        let ksi = &[0.0, 0.0];
+        for (edge_keys, solution) in &edge_keys_and_solutions {
+            for edge_key in edge_keys {
+                let mut n = NormalVector::at_edge(&mesh, &boundary, *edge_key)?;
+                n.evaluate(ksi)?;
+                assert_vec_approx_eq!(n.value.as_data(), solution, 1e-14);
+            }
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn at_edge_and_evaluate_work_qua16() -> Result<(), StrError> {
+        // 29---34----31---28---44---42----40
+        //  |               |               |
+        // 32   39    38   33   48   47    43
+        //  |               |               |
+        // 35   36    37   30   45   46    41
+        //  |               |               |
+        //  3---10-----6----2---23---20----17
+        //  |               |               |
+        //  7   15    14    9   27   26    22
+        //  |               |               |
+        // 11   12    13    5   24   25    19
+        //  |               |               |
+        //  0----4-----8----1---18---21----16
+        let mesh = Samples::block_2d_four_qua16();
+        let shapes = Shapes::new(&mesh)?;
+        let boundary = Boundary::new(&mesh, &shapes)?;
+
+        // the magnitude (l) of the normal vector should be equal to
+        // 0.75 = edge_length / 2.0 where 2.0 corresponds to the edge_length in the reference system
+        let l = 0.75; // magnitude of normal vector
+
+        // edge keys and correct normal vectors (solutions)
+        let edge_keys_and_solutions = [
+            // bottom
+            (vec![(0, 1), (1, 16)], [0.0, -l]),
+            // right
+            (vec![(16, 17), (17, 40)], [l, 0.0]),
+            // top
+            (vec![(28, 29), (28, 40)], [0.0, l]),
+            // left
+            (vec![(0, 3), (3, 29)], [-l, 0.0]),
+        ];
+
+        // check if the normal vectors at boundary are outward
+        let ksi = &[0.0, 0.0];
+        for (edge_keys, solution) in &edge_keys_and_solutions {
+            for edge_key in edge_keys {
+                let mut n = NormalVector::at_edge(&mesh, &boundary, *edge_key)?;
+                n.evaluate(ksi)?;
+                assert_vec_approx_eq!(n.value.as_data(), solution, 1e-14);
+            }
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn at_edge_and_evaluate_work_qua17() -> Result<(), StrError> {
+        // 30---38---35---32---29---47---45---43---41
+        //  |                   |                   |
+        // 33                  37                  46
+        //  |                   |                   |
+        // 36        40        34        48        44
+        //  |                   |                   |
+        // 39                  31                  42
+        //  |                   |                   |
+        //  3---14---10----6----2---27---24---21---18
+        //  |                   |                   |
+        //  7                  13                  26
+        //  |                   |                   |
+        // 11        16         9        28        23
+        //  |                   |                   |
+        // 15                   5                  20
+        //  |                   |                   |
+        //  0----4----8---12----1---19---22---25---17
+        let mesh = Samples::block_2d_four_qua17();
+        let shapes = Shapes::new(&mesh)?;
+        let boundary = Boundary::new(&mesh, &shapes)?;
+
+        // the magnitude (l) of the normal vector should be equal to
+        // 1.0 = edge_length / 2.0 where 2.0 corresponds to the edge_length in the reference system
+        let l = 1.0; // magnitude of normal vector
+
+        // edge keys and correct normal vectors (solutions)
+        let edge_keys_and_solutions = [
+            // bottom
+            (vec![(0, 1), (1, 17)], [0.0, -l]),
+            // right
+            (vec![(17, 18), (18, 41)], [l, 0.0]),
+            // top
+            (vec![(29, 30), (29, 41)], [0.0, l]),
+            // left
+            (vec![(0, 3), (3, 30)], [-l, 0.0]),
+        ];
+
+        // check if the normal vectors at boundary are outward
+        let ksi = &[0.0, 0.0];
+        for (edge_keys, solution) in &edge_keys_and_solutions {
+            for edge_key in edge_keys {
+                let mut n = NormalVector::at_edge(&mesh, &boundary, *edge_key)?;
+                n.evaluate(ksi)?;
+                assert_vec_approx_eq!(n.value.as_data(), solution, 1e-14);
+            }
+        }
         Ok(())
     }
 
@@ -205,6 +443,77 @@ mod tests {
             (vec![(0, 1, 2, 3)], [0.0, 0.0, -l]),
             // top
             (vec![(8, 9, 10, 11)], [0.0, 0.0, l]),
+        ];
+
+        let ksi = &[0.0, 0.0, 0.0];
+        for (face_keys, solution) in &face_keys_and_solutions {
+            for face_key in face_keys {
+                let mut n = NormalVector::at_face(&mesh, &boundary, *face_key)?;
+                n.evaluate(ksi)?;
+                assert_vec_approx_eq!(n.value.as_data(), solution, 1e-15);
+            }
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn at_face_and_evaluate_work_hex20() -> Result<(), StrError> {
+        //              51--------58--------54--------74--------71
+        //              /.                  /.                  /|
+        //             / .                 / .                 / |
+        //           55  .               57  .               73  |
+        //           /   .               /   .               /   |
+        //          /    .              /    .              /    |
+        //        52--------56--------53--------72--------70     |
+        //        /.     .            /.     .            /|     |
+        //       / .    59           / .    62           / |    76
+        //     65  .     .         67  .     .         79  |     |
+        //     /   .     .         /   .     .         /   |     |
+        //    /    .     .        /    .     .        /    |     |
+        //  63========66========64========78========77     |     |
+        //   |     .     .       |     .     .       |     |     |
+        //   |    60     .       |    61     .       |    75     |
+        //   |     .     4 - - - |15 - . - - 7 - - - |41 - | - -35
+        //   |     .    /.       |     .    /.       |     |    /|
+        //   |     .   / .       |     .   / .       |     |   / |
+        //   |     . 12  .       |     . 14  .       |     | 40  |
+        //   |     . /   .       |     . /   .       |     | /   |
+        //  68     ./    .      69     ./    .      80     |/    |
+        //   |     5 - - - -13 - | - - 6 - - - -39 - | - -34     |
+        //   |    /.     .       |    /.     .       |    /|     |
+        //   |   / .    16       |   / .    19       |   / |    43
+        //   | 27  .     .       | 29  .     .       | 49  |     |
+        //   | /   .     .       | /   .     .       | /   |     |
+        //   |/    .     .       |/    .     .       |/    |     |
+        //  22========28========23========48========45     |     |
+        //   |     .     .       |     .     .       |     |     |
+        //   |    17     .       |    18     .       |    42     |
+        //   |     .     0 - - - |11 - . - - 3 - - - |38 - | - -33
+        //   |     .    /        |     .    /        |     |    /
+        //   |     .   /         |     .   /         |     |   /
+        //   |     .  8          |     . 10          |     | 37
+        //   |     . /           |     . /           |     | /
+        //  30     ./           31     ./           50     |/
+        //   |     1 - - - - 9 - | - - 2 - - - -36 - | - -32
+        //   |    /              |    /              |    /
+        //   |   /               |   /               |   /
+        //   | 24                | 26                | 47
+        //   | /                 | /                 | /
+        //   |/                  |/                  |/
+        //  20========25========21========46========44
+        let mesh = Samples::block_3d_eight_hex20();
+        let shapes = Shapes::new(&mesh)?;
+        let boundary = Boundary::new(&mesh, &shapes)?;
+
+        // the magnitude (l) of the normal vector should be equal to
+        // face_area / 4.0 where 4.0 corresponds to the face_area in the reference system
+
+        // face keys and correct normal vectors (solutions)
+        let face_keys_and_solutions = [
+            (vec![(0, 1, 2, 3)], [0.0, 0.0, -1.0 / 4.0]),
+            (vec![(52, 53, 63, 64)], [0.0, 0.0, 1.0 / 4.0]),
+            (vec![(4, 7, 51, 54)], [-2.0 / 4.0, 0.0, 0.0]),
+            (vec![(34, 45, 70, 77)], [0.0, 2.0 / 4.0, 0.0]),
         ];
 
         let ksi = &[0.0, 0.0, 0.0];
