bbf49916db06114eb8c84faee2797e895c5ced09
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-05-25 22:11:58 +1000

    Rename file matrix_cases

--- src/integ/mod.rs
+++ src/integ/mod.rs
@@ -1,6 +1,6 @@
 //! Implements functions to perform numerical integration using Shapes
 
-mod matrix_output;
+mod matrix_cases;
 mod vector_cases;
-pub use crate::integ::matrix_output::*;
+pub use crate::integ::matrix_cases::*;
 pub use crate::integ::vector_cases::*;
