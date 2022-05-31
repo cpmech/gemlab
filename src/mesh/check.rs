0a971a16b9e7560d562af4bda5e5a4814491beba
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-05-31 20:36:34 +1000

    Impl check face normals

--- src/mesh/check.rs
+++ src/mesh/check.rs
@@ -1,4 +1,4 @@
-use super::{allocate_state, Edge, EdgeKey, Mesh};
+use super::{allocate_state, Edge, EdgeKey, Face, FaceKey, Mesh};
 use crate::shapes::Shape;
 use crate::StrError;
 use russell_lab::Vector;
@@ -38,7 +38,7 @@ pub fn check_jacobian(mesh: &Mesh) -> Result<(), StrError> {
     Ok(())
 }
 
-/// Checks if normal vectors of some edges are correct
+/// Checks if normal vectors of some 2D edges are correct
 pub fn check_2d_edge_normals(
     mesh: &Mesh,
     edges: &HashMap<EdgeKey, Edge>,
@@ -62,12 +62,36 @@ pub fn check_2d_edge_normals(
     Ok(())
 }
 
+/// Checks if normal vectors of some faces are correct
+pub fn check_face_normals(
+    mesh: &Mesh,
+    faces: &HashMap<FaceKey, Face>,
+    solutions: &HashMap<FaceKey, [f64; 3]>,
+    tolerance: f64,
+) -> Result<(), StrError> {
+    const GEO_NDIM: usize = 2;
+    let ksi = &[0.0, 0.0, 0.0];
+    let mut normal = Vector::new(mesh.space_ndim);
+    for (face_key, solution) in solutions {
+        let face = faces.get(face_key).ok_or("cannot find face_key in faces map")?;
+        let shape = Shape::new(mesh.space_ndim, GEO_NDIM, face.points.len())?;
+        let mut state = allocate_state(mesh, GEO_NDIM, &face.points)?;
+        shape.calc_boundary_normal(&mut normal, &mut state, ksi)?;
+        for i in 0..mesh.space_ndim {
+            if f64::abs(normal[i] - solution[i]) > tolerance {
+                return Err("wrong normal vector found");
+            }
+        }
+    }
+    Ok(())
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[cfg(test)]
 mod tests {
     use super::{check_2d_edge_normals, check_ids, check_jacobian};
-    use crate::mesh::{Cell, Edge, EdgeKey, Mesh, Point};
+    use crate::mesh::{check_face_normals, Cell, Edge, EdgeKey, Face, FaceKey, Mesh, Point};
     use std::collections::HashMap;
 
     #[test]
@@ -219,6 +243,78 @@ mod tests {
         assert_eq!(
             check_2d_edge_normals(&mesh, &edges, &solutions, 1e-15).err(),
             Some("cannot find edge_key in edges map")
+        );
+    }
+
+    #[test]
+    fn check_face_normals_works() {
+        //       4--------------7  1.0
+        //      /.             /|
+        //     / .            / |
+        //    /  .           /  |
+        //   /   .          /   |
+        //  5--------------6    |          z
+        //  |    .         |    |          ↑
+        //  |    0---------|----3  0.0     o → y
+        //  |   /          |   /          ↙
+        //  |  /           |  /          x
+        //  | /            | /
+        //  |/             |/
+        //  1--------------2   1.0
+        // 0.0            1.0
+        #[rustfmt::skip]
+        let mesh = Mesh {
+            space_ndim: 3,
+            points: vec![
+                Point { id: 0, coords: vec![0.0, 0.0, 0.0] },
+                Point { id: 1, coords: vec![1.0, 0.0, 0.0] },
+                Point { id: 2, coords: vec![1.0, 1.0, 0.0] },
+                Point { id: 3, coords: vec![0.0, 1.0, 0.0] },
+                Point { id: 4, coords: vec![0.0, 0.0, 1.0] },
+                Point { id: 5, coords: vec![1.0, 0.0, 1.0] },
+                Point { id: 6, coords: vec![1.0, 1.0, 1.0] },
+                Point { id: 7, coords: vec![0.0, 1.0, 1.0] },
+            ],
+            cells: vec![
+                Cell { id: 0, attribute_id: 1, geo_ndim: 3, points: vec![0,1,2,3, 4,5,6,7] },
+            ],
+        };
+
+        // the magnitude (l) of the normal vector should be equal to
+        // 0.25 = face_area / 4.0 where 4.0 corresponds to the face_area in the reference system
+        let l = 0.25; // magnitude of normal vector
+
+        #[rustfmt::skip]
+        let mut faces: HashMap<FaceKey, Face> = HashMap::from([
+            ((0, 3, 4, 7), Face { points: vec![0, 4, 7, 3] }),
+            ((1, 2, 5, 6), Face { points: vec![1, 2, 6, 5] }),
+            ((0, 1, 4, 5), Face { points: vec![0, 1, 5, 4] }),
+            ((2, 3, 6, 7), Face { points: vec![2, 3, 7, 6] }),
+            ((0, 1, 2, 3), Face { points: vec![0, 3, 2, 1] }),
+            ((4, 5, 6, 7), Face { points: vec![4, 5, 6, 7] }),
+        ]);
+        let solutions: HashMap<FaceKey, [f64; 3]> = HashMap::from([
+            ((0, 3, 4, 7), [-l, 0.0, 0.0]),
+            ((1, 2, 5, 6), [l, 0.0, 0.0]),
+            ((0, 1, 4, 5), [0.0, -l, 0.0]),
+            ((2, 3, 6, 7), [0.0, l, 0.0]),
+            ((0, 1, 2, 3), [0.0, 0.0, -l]),
+            ((4, 5, 6, 7), [0.0, 0.0, l]),
+        ]);
+        assert_eq!(check_face_normals(&mesh, &faces, &solutions, 1e-15).expect("ok"), ());
+
+        let points = &mut faces.get_mut(&(0, 3, 4, 7)).unwrap().points;
+        points[0] = 4;
+        points[1] = 0;
+        assert_eq!(
+            check_face_normals(&mesh, &faces, &solutions, 1e-15).err(),
+            Some("wrong normal vector found")
+        );
+
+        let solutions: HashMap<FaceKey, [f64; 3]> = HashMap::from([((10, 20, 30, 40), [0.0, 0.0, l])]);
+        assert_eq!(
+            check_face_normals(&mesh, &faces, &solutions, 1e-15).err(),
+            Some("cannot find face_key in faces map")
         );
     }
 }
