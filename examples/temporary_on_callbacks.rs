00d8e878f6d5bb408c80b7d10b7808f4c659f8d5
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-06-10 17:33:14 +1000

    Improve study on callbacks

--- examples/temporary_on_callbacks.rs
+++ examples/temporary_on_callbacks.rs
@@ -1,43 +1,46 @@
+use gemlab::StrError;
 use russell_lab::Vector;
 
 struct Processor<'a> {
-    callback: Box<dyn FnMut(&mut Vector, &[f64]) + 'a>,
+    callback: Option<Box<dyn FnMut(&mut Vector, &[f64]) -> Result<(), StrError> + 'a>>,
 }
 
 impl<'a> Processor<'a> {
-    fn set_callback(&mut self, c: impl FnMut(&mut Vector, &[f64]) + 'a) {
-        self.callback = Box::new(c);
+    fn set_callback(&mut self, c: impl FnMut(&mut Vector, &[f64]) -> Result<(), StrError> + 'a) {
+        self.callback = Some(Box::new(c));
     }
 
-    fn process_events(&mut self) {
+    fn process_events(&mut self) -> Result<(), StrError> {
         let ksi = vec![0.0; 2];
         let mut x = Vector::new(2);
-        (self.callback)(&mut x, &ksi);
+        // (self.callback)(&mut x, &ksi);
+        match &mut self.callback {
+            Some(a) => a(&mut x, &ksi)?,
+            None => (),
+        }
         println!("got x =\n{}", x);
+        Ok(())
     }
 }
 
-fn simple_callback(x: &mut Vector, ksi: &[f64]) {
-    println!("ksi = {:?}", ksi);
-    println!("{}", x);
+fn simple_callback(_: &mut Vector, _: &[f64]) -> Result<(), StrError> {
+    Err("nothing to see here")
 }
 
-fn main() {
+fn main() -> Result<(), StrError> {
     let mut p = Processor {
-        callback: Box::new(simple_callback),
+        callback: Some(Box::new(simple_callback)),
     };
-    p.process_events();
+    println!("{:?}", p.process_events().err());
     let v = Vector::from(&[1.0, 2.0]);
-    let x0 = 1.0;
-    let x1 = 2.0;
     let my_fun = move |x: &mut Vector, ksi: &[f64]| {
         println!("using ksi = {:?}", ksi);
         x[0] = v[0];
         x[1] = v[1];
+        Ok(())
     };
-    println!("x0 = {}", x0);
-    println!("x1 = {}", x1);
     // println!("v = {:?}", v);
     p.set_callback(my_fun);
-    p.process_events();
+    p.process_events()?;
+    Ok(())
 }
