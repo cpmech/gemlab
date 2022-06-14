cbe5048de9bf85fe8584bae2bcd414576b1b1f5c
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-06-14 21:56:47 +1000

    Impl quarter disk 3d

--- src/mesh/generators.rs
+++ src/mesh/generators.rs
@@ -1,4 +1,4 @@
-use super::{join_meshes, ArgsRing, Block, Constraint2D, Mesh};
+use super::{join_meshes, ArgsRing, Block, Constraint2D, Constraint3D, Mesh};
 use crate::shapes::GeoKind;
 use crate::util::{COS_PI_BY_8, ONE_BY_SQRT_2, PI, SIN_PI_BY_8, SQRT_2};
 use crate::StrError;
@@ -187,35 +187,38 @@ impl Structured {
         let m = a / 2.0;
         let n = a + b / 2.0;
         let e = n * COS_PI_BY_4;
+        #[rustfmt::skip]
         let mut block_1 = Block::new(&[
             [0.0, 0.0],
-            [a, 0.0],
-            [c2, c2],
-            [0.0, a],
-            [m, 0.0],
-            [c3, c1],
-            [c1, c3],
-            [0.0, m],
+            [  a, 0.0],
+            [ c2,  c2],
+            [0.0,   a],
+            [  m, 0.0],
+            [ c3,  c1],
+            [ c1,  c3],
+            [0.0,   m],
         ])?;
+        #[rustfmt::skip]
         let mut block_2 = Block::new(&[
-            [a, 0.0],
-            [r, 0.0],
-            [d2, d2],
-            [c2, c2],
-            [n, 0.0],
-            [d3, d1],
-            [e, e],
-            [c3, c1],
+            [ a, 0.0],
+            [ r, 0.0],
+            [d2,  d2],
+            [c2,  c2],
+            [ n, 0.0],
+            [d3,  d1],
+            [ e,   e],
+            [c3,  c1],
         ])?;
+        #[rustfmt::skip]
         let mut block_3 = Block::new(&[
-            [0.0, a],
-            [c2, c2],
-            [d2, d2],
-            [0.0, r],
-            [c1, c3],
-            [e, e],
-            [d1, d3],
-            [0.0, n],
+            [0.0,  a],
+            [ c2, c2],
+            [ d2, d2],
+            [0.0,  r],
+            [ c1, c3],
+            [ e,   e],
+            [ d1, d3],
+            [0.0,  n],
         ])?;
         block_1.set_ndiv(&[ndiv_a, ndiv_a])?;
         block_2.set_ndiv(&[ndiv_b, ndiv_a])?;
@@ -223,6 +226,219 @@ impl Structured {
         let ct = Constraint2D::Circle(0.0, 0.0, r);
         block_2.set_edge_constraint(1, Some(ct.clone()))?;
         block_3.set_edge_constraint(2, Some(ct))?;
+        let mesh_1 = block_1.subdivide(target)?;
+        let mesh_2 = block_2.subdivide(target)?;
+        let mesh_3 = block_3.subdivide(target)?;
+        let mesh_1_2 = join_meshes(&mesh_1, &mesh_2)?;
+        join_meshes(&mesh_1_2, &mesh_3)
+    }
+
+    /// Generates a mesh representing a quarter of a disk in 3D (A-version)
+    ///
+    /// ```text
+    /// y ^
+    ///   .
+    ///   #**=---__
+    ///   |        '*._
+    ///   |            *._
+    ///   |               *.
+    ///   |                 *.
+    ///   |                   *
+    ///   |                    *
+    ///   |                     *
+    ///   |                      *
+    ///   |                       *
+    ///   |                       #
+    ///   o-----------------------# --> x
+    ///                        radius
+    /// ```
+    ///
+    /// # Input
+    ///
+    /// * `r` -- radius
+    /// * `z` -- thickness
+    /// * `ndiv` -- number of divisions along on the x-y plane (must be > 0)
+    /// * `ndiv_z` -- number of divisions along thickness (must be > 0)
+    /// * `target` -- Hex shapes only
+    pub fn quarter_disk_3d_a(r: f64, z: f64, ndiv: usize, ndiv_z: usize, target: GeoKind) -> Result<Mesh, StrError> {
+        let m = r / 2.0;
+        let n = r / SQRT_2;
+        let p = 1.15 * m / SQRT_2;
+        #[rustfmt::skip]
+        let mut block_1 = Block::new(&[
+            [0.0, 0.0, 0.0],
+            [  m, 0.0, 0.0],
+            [  p,   p, 0.0],
+            [0.0,   m, 0.0],
+            [0.0, 0.0,   z],
+            [  m, 0.0,   z],
+            [  p,   p,   z],
+            [0.0,   m,   z],
+        ])?;
+        #[rustfmt::skip]
+        let mut block_2 = Block::new(&[
+            [m, 0.0, 0.0],
+            [r, 0.0, 0.0],
+            [n,   n, 0.0],
+            [p,   p, 0.0],
+            [m, 0.0,   z],
+            [r, 0.0,   z],
+            [n,   n,   z],
+            [p,   p,   z],
+        ])?;
+        #[rustfmt::skip]
+        let mut block_3 = Block::new(&[
+            [0.0, m, 0.0],
+            [  p, p, 0.0],
+            [  n, n, 0.0],
+            [0.0, r, 0.0],
+            [0.0, m,   z],
+            [  p, p,   z],
+            [  n, n,   z],
+            [0.0, r,   z],
+        ])?;
+        block_1.set_ndiv(&[ndiv, ndiv, ndiv_z])?;
+        block_2.set_ndiv(&[ndiv, ndiv, ndiv_z])?;
+        block_3.set_ndiv(&[ndiv, ndiv, ndiv_z])?;
+        let ct = Constraint3D::CylinderZ(0.0, 0.0, r);
+        block_2.set_face_constraint(1, Some(ct.clone()))?;
+        block_3.set_face_constraint(3, Some(ct))?;
+        let mesh_1 = block_1.subdivide(target)?;
+        let mesh_2 = block_2.subdivide(target)?;
+        let mesh_3 = block_3.subdivide(target)?;
+        let mesh_1_2 = join_meshes(&mesh_1, &mesh_2)?;
+        join_meshes(&mesh_1_2, &mesh_3)
+    }
+
+    /// Generates a mesh representing a quarter of a disk in 3D (B-version)
+    ///
+    /// ```text
+    /// y ^
+    ///   .
+    ///   #**=---__
+    ///   |        '*._
+    ///   |            *._
+    ///   |               *.
+    ///   |                 *.
+    ///   |                   *
+    ///   |                    *
+    ///   |                     *
+    ///   |                      *
+    ///   |                       *
+    ///   |                       #
+    ///   o-----------------------# --> x
+    ///                        radius
+    ///   |<----a---->|<----b---->|
+    ///   |<----------r---------->|
+    /// ```
+    ///
+    /// # Input
+    ///
+    /// * `r` -- the radius
+    /// * `z` -- thickness
+    /// * `ndiv_a` -- number of divisions for 'a' on the x-y plane (must be > 0)
+    /// * `ndiv_b` -- number of divisions for 'b' on the x-y plane (must be > 0)
+    /// * `ndiv_z` -- number of divisions along thickness (must be > 0)
+    /// * `target` -- Hex shapes only
+    pub fn quarter_disk_3d_b(
+        a: f64,
+        r: f64,
+        z: f64,
+        ndiv_a: usize,
+        ndiv_b: usize,
+        ndiv_z: usize,
+        target: GeoKind,
+    ) -> Result<Mesh, StrError> {
+        if a >= r {
+            return Err("'a' must be smaller than 'r'");
+        }
+        const COS_PI_BY_4: f64 = ONE_BY_SQRT_2;
+        let b = r - a;
+        let c1 = a * SIN_PI_BY_8;
+        let c2 = a * COS_PI_BY_4;
+        let c3 = a * COS_PI_BY_8;
+        let d1 = r * SIN_PI_BY_8;
+        let d2 = r * COS_PI_BY_4;
+        let d3 = r * COS_PI_BY_8;
+        let m = a / 2.0;
+        let n = a + b / 2.0;
+        let e = n * COS_PI_BY_4;
+        let f = z / 2.0;
+        #[rustfmt::skip]
+        let mut block_1 = Block::new(&[
+            [0.0, 0.0, 0.0],
+            [  a, 0.0, 0.0],
+            [ c2,  c2, 0.0],
+            [0.0,   a, 0.0],
+            [0.0, 0.0,   z],
+            [  a, 0.0,   z],
+            [ c2,  c2,   z],
+            [0.0,   a,   z],
+            [  m, 0.0, 0.0],
+            [ c3,  c1, 0.0],
+            [ c1,  c3, 0.0],
+            [0.0,   m, 0.0],
+            [  m, 0.0,   z],
+            [ c3,  c1,   z],
+            [ c1,  c3,   z],
+            [0.0,   m,   z],
+            [0.0, 0.0,   f],
+            [  a, 0.0,   f],
+            [ c2,  c2,   f],
+            [0.0,   a,   f],
+        ])?;
+        #[rustfmt::skip]
+        let mut block_2 = Block::new(&[
+            [ a, 0.0, 0.0],
+            [ r, 0.0, 0.0],
+            [d2,  d2, 0.0],
+            [c2,  c2, 0.0],
+            [ a, 0.0,   z],
+            [ r, 0.0,   z],
+            [d2,  d2,   z],
+            [c2,  c2,   z],
+            [ n, 0.0, 0.0],
+            [d3,  d1, 0.0],
+            [ e,   e, 0.0],
+            [c3,  c1, 0.0],
+            [ n, 0.0,   z],
+            [d3,  d1,   z],
+            [ e,   e,   z],
+            [c3,  c1,   z],
+            [ a, 0.0,   f],
+            [ r, 0.0,   f],
+            [d2,  d2,   f],
+            [c2,  c2,   f],
+        ])?;
+        #[rustfmt::skip]
+        let mut block_3 = Block::new(&[
+            [0.0,  a, 0.0],
+            [ c2, c2, 0.0],
+            [ d2, d2, 0.0],
+            [0.0,  r, 0.0],
+            [0.0,  a,   z],
+            [ c2, c2,   z],
+            [ d2, d2,   z],
+            [0.0,  r,   z],
+            [ c1, c3, 0.0],
+            [ e,   e, 0.0],
+            [ d1, d3, 0.0],
+            [0.0,  n, 0.0],
+            [ c1, c3,   z],
+            [ e,   e,   z],
+            [ d1, d3,   z],
+            [0.0,  n,   z],
+            [0.0,  a,   f],
+            [ c2, c2,   f],
+            [ d2, d2,   f],
+            [0.0,  r,   f],
+        ])?;
+        block_1.set_ndiv(&[ndiv_a, ndiv_a, ndiv_z])?;
+        block_2.set_ndiv(&[ndiv_b, ndiv_a, ndiv_z])?;
+        block_3.set_ndiv(&[ndiv_a, ndiv_b, ndiv_z])?;
+        let ct = Constraint3D::CylinderZ(0.0, 0.0, r);
+        block_2.set_face_constraint(1, Some(ct.clone()))?;
+        block_3.set_face_constraint(3, Some(ct))?;
         let mesh_1 = block_1.subdivide(target)?;
         let mesh_2 = block_2.subdivide(target)?;
         let mesh_3 = block_3.subdivide(target)?;
@@ -382,6 +598,46 @@ mod tests {
         }
         if false {
             draw_mesh(mesh, true, "/tmp/gemlab/test_quarter_disk_2d_b_qua16.svg")?;
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn quarter_disk_3d_a_works_hex32() -> Result<(), StrError> {
+        let mesh = Structured::quarter_disk_3d_a(6.0, 1.5, 1, 1, GeoKind::Hex32)?;
+        check_overlapping_points(&mesh, 0.02)?;
+        assert_eq!(mesh.cells.len(), 3);
+        for p in [
+            32, 38, 39, 33, 54, 55, 52, // z-min
+            48, 50, 62, // z-1/3
+            49, 51, 63, // z-2/3
+            34, 44, 45, 35, 58, 59, 53, // z-max
+        ] {
+            let d = point_point_distance(&mesh.points[p].coords[0..2], &[0.0, 0.0])?;
+            assert_approx_eq!(d, 6.0, 1e-15);
+        }
+        if false {
+            draw_mesh(mesh, true, "/tmp/gemlab/test_quarter_disk_3d_a_hex32.svg")?;
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn quarter_disk_3d_b_works_hex32() -> Result<(), StrError> {
+        let mesh = Structured::quarter_disk_3d_b(3.0, 6.0, 1.5, 1, 1, 1, GeoKind::Hex32)?;
+        check_overlapping_points(&mesh, 0.02)?;
+        assert_eq!(mesh.cells.len(), 3);
+        for p in [
+            32, 38, 39, 33, 54, 55, 52, // z-min
+            48, 50, 62, // z-1/3
+            49, 51, 63, // z-2/3
+            34, 44, 45, 35, 58, 59, 53, // z-max
+        ] {
+            let d = point_point_distance(&mesh.points[p].coords[0..2], &[0.0, 0.0])?;
+            assert_approx_eq!(d, 6.0, 1e-15);
+        }
+        if false {
+            draw_mesh(mesh, true, "/tmp/gemlab/test_quarter_disk_3d_b_hex32.svg")?;
         }
         Ok(())
     }
