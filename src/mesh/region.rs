b14ac9e80f4dbdd3f050b4b4e7b218e8be2ccdae
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-06-15 16:40:40 +1000

    Improve doc examples

--- src/mesh/region.rs
+++ src/mesh/region.rs
@@ -1,7 +1,165 @@
 use super::{Extract, Features, Find, MapEdge2dToCells, MapFaceToCells, Mesh};
 use crate::StrError;
 
-/// Holds all (immutable) data related to a mesh including element shapes, boundary, interior, and functions to find features
+/// Holds all (immutable) data related to a mesh and functions to find features (points, edges, faces)
+///
+/// # Examples
+///
+/// ## Two-dimensions
+///
+/// ```
+/// use gemlab::mesh::{At, Cell, Extract, Mesh, Point, Region};
+/// use gemlab::shapes::GeoKind;
+/// use gemlab::StrError;
+///
+/// fn main() -> Result<(), StrError> {
+///     //  3---------2---------5
+///     //  |         |         |
+///     //  |   [0]   |   [1]   |
+///     //  |         |         |
+///     //  0---------1---------4
+///     let mesh = Mesh {
+///         ndim: 2,
+///         #[rustfmt::skip]
+///          points: vec![
+///              Point { id: 0, coords: vec![0.0, 0.0] },
+///              Point { id: 1, coords: vec![1.0, 0.0] },
+///              Point { id: 2, coords: vec![1.0, 1.0] },
+///              Point { id: 3, coords: vec![0.0, 1.0] },
+///              Point { id: 4, coords: vec![2.0, 0.0] },
+///              Point { id: 5, coords: vec![2.0, 1.0] },
+///          ],
+///         cells: vec![
+///             Cell {
+///                 id: 0,
+///                 attribute_id: 1,
+///                 kind: GeoKind::Qua4,
+///                 points: vec![0, 1, 2, 3],
+///             },
+///             Cell {
+///                 id: 1,
+///                 attribute_id: 2,
+///                 kind: GeoKind::Qua4,
+///                 points: vec![1, 4, 5, 2],
+///             },
+///         ],
+///     };
+///
+///     let region = Region::with(&mesh, Extract::Boundary)?;
+///
+///     // check features
+///
+///     let mut points: Vec<_> = region.features.points.iter().copied().collect();
+///     points.sort();
+///     assert_eq!(points, [0, 1, 2, 3, 4, 5]);
+///
+///     let mut edges: Vec<_> = region.features.edges.keys().copied().collect();
+///     edges.sort();
+///     assert_eq!(edges, [(0, 1), (0, 3), (1, 4), (2, 3), (2, 5), (4, 5)]);
+///
+///     // find features
+///
+///     let mut points: Vec<_> = region.find.points(At::X(2.0))?.iter().copied().collect();
+///     points.sort();
+///     assert_eq!(points, &[4, 5]);
+///
+///     let mut edges: Vec<_> = region.find.edges(At::Y(1.0))?.iter().copied().collect();
+///     edges.sort();
+///     assert_eq!(edges, &[(2, 3), (2, 5)]);
+///
+///     Ok(())
+/// }
+/// ```
+///
+/// ## Three-dimensions
+///
+/// ```
+/// use gemlab::mesh::{At, Cell, Extract, Mesh, Point, Region};
+/// use gemlab::shapes::GeoKind;
+/// use gemlab::StrError;
+///
+/// fn main() -> Result<(), StrError> {
+///     //          .4--------------7
+///     //        ,' |            ,'|
+///     //      ,'              ,'  |
+///     //    ,'     |        ,'    |
+///     //  5'==============6'      |
+///     //  |               |       |
+///     //  |        |      |       |
+///     //  |       ,0- - - | - - - 3
+///     //  |     ,'        |     ,'
+///     //  |   ,'          |   ,'
+///     //  | ,'            | ,'
+///     //  1'--------------2'
+///     #[rustfmt::skip]
+///      let mesh = Mesh {
+///          ndim: 3,
+///          points: vec![
+///              Point { id: 0, coords: vec![0.0, 0.0, 0.0] },
+///              Point { id: 1, coords: vec![1.0, 0.0, 0.0] },
+///              Point { id: 2, coords: vec![1.0, 1.0, 0.0] },
+///              Point { id: 3, coords: vec![0.0, 1.0, 0.0] },
+///              Point { id: 4, coords: vec![0.0, 0.0, 1.0] },
+///              Point { id: 5, coords: vec![1.0, 0.0, 1.0] },
+///              Point { id: 6, coords: vec![1.0, 1.0, 1.0] },
+///              Point { id: 7, coords: vec![0.0, 1.0, 1.0] },
+///          ],
+///          cells: vec![
+///              Cell { id: 0, attribute_id: 1, kind: GeoKind::Hex8,
+///                                  points: vec![0,1,2,3, 4,5,6,7] },
+///          ],
+///      };
+///
+///     let region = Region::with(&mesh, Extract::Boundary)?;
+///
+///     // check features
+///
+///     let mut points: Vec<_> = region.features.points.iter().copied().collect();
+///     points.sort();
+///     assert_eq!(points, (0..8).collect::<Vec<_>>());
+///
+///     let mut edges: Vec<_> = region.features.edges.keys().copied().collect();
+///     edges.sort();
+///     #[rustfmt::skip]
+///     assert_eq!(
+///         edges,
+///         [
+///             (0, 1), (0, 3), (0, 4), (1, 2),
+///             (1, 5), (2, 3), (2, 6), (3, 7),
+///             (4, 5), (4, 7), (5, 6), (6, 7)
+///         ]
+///     );
+///
+///     let mut faces: Vec<_> = region.features.faces.keys().copied().collect();
+///     faces.sort();
+///     assert_eq!(
+///         faces,
+///         [
+///             (0, 1, 2, 3),
+///             (0, 1, 4, 5),
+///             (0, 3, 4, 7),
+///             (1, 2, 5, 6),
+///             (2, 3, 6, 7),
+///             (4, 5, 6, 7),
+///         ]
+///     );
+///
+///     // find features
+///
+///     let mut points: Vec<_> = region.find.points(At::XY(1.0, 1.0))?.iter().copied().collect();
+///     points.sort();
+///     assert_eq!(points, &[2, 6]);
+///
+///     let mut edges: Vec<_> = region.find.edges(At::YZ(1.0, 1.0))?.iter().copied().collect();
+///     edges.sort();
+///     assert_eq!(edges, &[(6, 7)]);
+///
+///     let mut faces: Vec<_> = region.find.faces(At::Y(1.0))?.iter().copied().collect();
+///     faces.sort();
+///     assert_eq!(faces, &[(2, 3, 6, 7)]);
+///     Ok(())
+/// }
+/// ```
 pub struct Region<'a> {
     /// Holds the raw mesh data
     pub mesh: &'a Mesh,
