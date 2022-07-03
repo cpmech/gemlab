a9ac0f21da73f09240698f952a31de2c2d5bc920
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-07-03 14:42:16 +1000

    Impl coords/tris generation in example

--- examples/grid_search_gen_triangles.rs
+++ examples/grid_search_gen_triangles.rs
@@ -1,10 +1,15 @@
 use gemlab::util::SQRT_3;
 use gemlab::StrError;
 use plotpy::{Canvas, Plot, PolyCode};
+use std::collections::HashMap;
 use std::fmt::Write;
 use std::fs;
 
-const APPROXIMATED: bool = true;
+/// Generates approximated equilateral triangles instead of exact equilateral triangles
+const APPROXIMATE_EQUILATERAL_TRIANGLE: bool = true;
+
+/// Generates the coordinates/triangles lists instead of a single lists with the (repeated) triangle coordinates
+const GEN_COORDINATES_TRIANGLES_LISTS: bool = true;
 
 // Equilateral triangle
 //
@@ -24,7 +29,11 @@ const APPROXIMATED: bool = true;
 // area = l * h / 2.0;
 fn generate_equilateral_triangle(l: f64, x0: f64, y0: f64, flipped: bool) -> Vec<[f64; 2]> {
     let s = l / 2.0;
-    let h = if APPROXIMATED { l * 1.7 / 2.0 } else { l * SQRT_3 / 2.0 };
+    let h = if APPROXIMATE_EQUILATERAL_TRIANGLE {
+        l * 1.7 / 2.0
+    } else {
+        l * SQRT_3 / 2.0
+    };
     if flipped {
         return vec![[x0, y0 + h], [x0 + s, y0], [x0 + l, y0 + h]];
     } else {
@@ -35,11 +44,15 @@ fn generate_equilateral_triangle(l: f64, x0: f64, y0: f64, flipped: bool) -> Vec
 fn main() -> Result<(), StrError> {
     // constants
     const L: f64 = 1.0;
-    const NX: usize = 10;
-    const NY: usize = 10;
+    const NX: usize = 2;
+    const NY: usize = 2;
     const X0: f64 = 0.0;
     const Y0: f64 = 0.0;
-    let h = if APPROXIMATED { L * 1.7 / 2.0 } else { L * SQRT_3 / 2.0 };
+    let h = if APPROXIMATE_EQUILATERAL_TRIANGLE {
+        L * 1.7 / 2.0
+    } else {
+        L * SQRT_3 / 2.0
+    };
 
     // generate triangles
     let mut y0 = Y0;
@@ -61,7 +74,12 @@ fn main() -> Result<(), StrError> {
     }
 
     // draw
-    let filekey = format!("example_grid_search_gen_triangles_{}", triangles.len());
+    let filekey = if GEN_COORDINATES_TRIANGLES_LISTS {
+        // ct means coordinates/triangles
+        format!("example_grid_search_gen_triangles_ct_{}", triangles.len())
+    } else {
+        format!("example_grid_search_gen_triangles_{}", triangles.len())
+    };
     let mut plot = Plot::new();
     draw_triangles(&mut plot, &triangles);
     plot.set_equal_axes(true)
@@ -71,14 +89,48 @@ fn main() -> Result<(), StrError> {
 
     // generate file for use in examples
     let mut buffer = String::new();
-    write!(&mut buffer, "{}\n", triangles.len()).unwrap();
-    for t in &triangles {
-        write!(
-            &mut buffer,
-            "{:?} {:?} {:?} {:?} {:?} {:?}\n",
-            t[0][0], t[0][1], t[1][0], t[1][1], t[2][0], t[2][1]
-        )
-        .unwrap();
+    if GEN_COORDINATES_TRIANGLES_LISTS {
+        const BIG: f64 = 1_000_000_000.0;
+        let mut key2coords = HashMap::new();
+        for t in &triangles {
+            for m in 0..3 {
+                let x = t[m][0];
+                let y = t[m][1];
+                assert!(x < BIG);
+                assert!(y < BIG);
+                let temperature = f64::sqrt(x * x + y * y);
+                let key = ((x * BIG) as usize, (y * BIG) as usize);
+                key2coords.insert(key, (x, y, temperature));
+            }
+        }
+        let mut point_keys: Vec<_> = key2coords.keys().copied().collect();
+        point_keys.sort();
+        write!(&mut buffer, "{}\n", point_keys.len()).unwrap();
+        for key in &point_keys {
+            let (x, y, temperature) = key2coords.get(&key).unwrap();
+            write!(&mut buffer, "{:?} {:?} {:?}\n", x, y, temperature).unwrap();
+        }
+        write!(&mut buffer, "{}\n", triangles.len()).unwrap();
+        for t in &triangles {
+            for m in 0..3 {
+                let x = t[m][0];
+                let y = t[m][1];
+                let key = ((x * BIG) as usize, (y * BIG) as usize);
+                let point_id = point_keys.iter().position(|k| *k == key).unwrap();
+                write!(&mut buffer, "{:?} ", point_id).unwrap();
+            }
+            write!(&mut buffer, "\n").unwrap();
+        }
+    } else {
+        write!(&mut buffer, "{}\n", triangles.len()).unwrap();
+        for t in &triangles {
+            write!(
+                &mut buffer,
+                "{:?} {:?} {:?} {:?} {:?} {:?}\n",
+                t[0][0], t[0][1], t[1][0], t[1][1], t[2][0], t[2][1]
+            )
+            .unwrap();
+        }
     }
     let path = format!("/tmp/gemlab/{}.dat", filekey);
     fs::write(path, buffer).expect("cannot write file");
