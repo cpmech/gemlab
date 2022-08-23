291eaf02d1137cc1250056ff2d170232b2a32e50
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-08-23 17:11:12 +1000

    Move FindCell to mesh module

--- src/mesh/find_cell.rs
+++ src/mesh/find_cell.rs
@@ -1,29 +1,37 @@
 #![allow(unused)]
 
-use super::{calc_container_key, AsArray2D, GS_DEFAULT_BORDER_TOL, GS_DEFAULT_TOLERANCE};
+use super::Mesh;
+use crate::util::calc_container_key;
 use crate::StrError;
 use plotpy::{Canvas, Plot, PolyCode, Text};
 use russell_stat::Histogram;
 use std::collections::{HashMap, HashSet};
 use std::fmt;
 
-/// Defines a bounding-box ratio coefficient in [0,1] to mark cells as "large" cells compared the others
+/// Default tolerance for all directions
+pub const FIND_CELL_TOLERANCE: f64 = 1e-4;
+
+/// Default border tolerance to handle imprecision near the borders
+pub const FIND_CELL_BORDER_TOL: f64 = 1e-2;
+
+/// Defines a bounding-box ratio in [0,1] to mark cells as "large" compared others
 ///
-/// Example: If a cell has a max(bounding box length) / largest >= 0.75,
+/// Example: If a cell has a max(bounding_box_length) / largest >= 0.75,
 ///          Then this cell is put into the large_cells list
 ///
 /// This constant enables the strategy of putting some cells in a selected list of large cells.
-/// The other constant [GS_LARGE_CELLS_PCT] also influences the algorithm by enabling or disabling this strategy.
-const GS_LARGE_CELLS_CUTOFF: f64 = 0.75;
+/// The other constant [FIND_CELL_LARGE_PCT] also influences the algorithm by enabling or disabling this strategy.
+const FIND_CELL_LARGE_CUTOFF: f64 = 0.75;
 
-/// Controls the count percentage in [0,1] of large cells allowed in the selected array of large cells.
+/// Controls the count percentage in [0,1] of large cells allowed.
 ///
-/// Example: If 20% of cells are too large, put them in a separated list of large cells;
+/// Example: If 20% of cells are too large, then we put them in a separated list of large cells;
 ///          otherwise, the mesh is homogeneous enough so put all cells in the containers.
 ///
-/// If the actual ratio is greater than this constant (i.e., many cells are large such as in a homogeneous mesh),
-/// the "strategy" of selecting large cells is abandoned. The other constant [GS_LARGE_CELLS_CUTOFF] also influences the algorithm.
-const GS_LARGE_CELLS_PCT: f64 = 0.2;
+/// If the actual ratio is greater than this constant (i.e., many cells are large such as
+/// in a homogeneous mesh), the "strategy" of selecting large cells is abandoned.
+/// The other constant [FIND_CELL_LARGE_CUTOFF] also influences the algorithm.
+const FIND_CELL_LARGE_PCT: f64 = 0.2;
 
 /// Specifies the key of containers (or bins in the grid)
 type ContainerKey = usize;
@@ -43,8 +51,8 @@ const I_MIN: usize = 0;
 const I_MAX: usize = 1;
 type BboxMinMax = Vec<Vec<f64>>; // [ndim][N_MIN_MAX]
 
-pub struct GridSearchCellNew {
-    ndim: usize,                     // space dimension
+pub struct FindCell<'a> {
+    mesh: &'a Mesh,                  // the mesh
     ndiv: Vec<usize>,                // (ndim) number of divisions along each direction
     xmin: Vec<f64>,                  // (ndim) min values
     xmax: Vec<f64>,                  // (ndim) max values
@@ -54,61 +62,36 @@ pub struct GridSearchCellNew {
     large_cells: Container,          // holds the CellId of large cells
 }
 
-impl GridSearchCellNew {
+impl<'a> FindCell<'a> {
     /// Allocates a new instance
     ///
     /// # Input
     ///
-    /// * `ndim` -- is the space dimension (2 or 3)
-    /// * `points` -- (npoint) is a list of coordinates such as `[[x0,y0,z0,...], [x1,y1,z1,...], [x2,y2,z2,...], [x3,y3,z3,...]]`
-    /// * `cells` -- (ncell) is a list of connectivity (topology) such as `[[0,2,1,3], [2,1,0,3]]`
     /// * `tolerance` -- is a tolerance to expand the bounding box of triangles and compare points; e.g. 1e-4
-    ///     - If None, [GS_DEFAULT_TOLERANCE] is used
+    ///     - If None, [FIND_CELL_TOLERANCE] is used
     /// * `border_tol` -- is a tolerance used to expand the border a little bit and then
     ///   accommodate eventual imprecision near the borders; e.g. 1e-2
-    ///     - If None, [GS_DEFAULT_BORDER_TOL] is used
-    /// * `large_cells_cutoff` -- is the ratio to define large cells compared to the largest bounding box length; e.g. 0.75
-    ///     - If None, [GS_LARGE_CELLS_CUTOFF] is used
-    /// * `large_cells_pct` -- is the ratio (percentage) of allowed large cells; e.g. 0.2
-    ///     - If None, [GS_LARGE_CELLS_PCT] is used
-    ///
-    /// # Panics
-    ///
-    /// If the list of points or cells contain more entries at the first row, a panic will occur.
-    /// The first row is used to compute the number of points/cells.
-    pub fn new<'a, P, C>(
-        ndim: usize,
-        points: &'a P,
-        cells: &'a C,
+    ///     - If None, [FIND_CELL_BORDER_TOL] is used
+    /// * `large_cutoff` -- is the ratio to define large cells compared to the largest bounding box length; e.g. 0.75
+    ///     - If None, [FIND_CELL_LARGE_CUTOFF] is used
+    /// * `large_pct` -- is the ratio (percentage) of allowed large cells; e.g. 0.2
+    ///     - If None, [FIND_CELL_LARGE_PCT] is used
+    pub fn new(
+        mesh: &'a Mesh,
         tolerance: Option<f64>,
         border_tol: Option<f64>,
-        large_cells_cutoff: Option<f64>,
-        large_cells_pct: Option<f64>,
-    ) -> Result<Self, StrError>
-    where
-        P: AsArray2D<'a, f64>,
-        C: AsArray2D<'a, usize>,
-    {
-        // check input
-        if ndim < 2 || ndim > 3 {
-            return Err("ndim must be 2 or 3");
-        }
-        let (npoint, ncol) = points.size();
-        let (ncell, nnode) = cells.size();
-        if npoint < 3 {
-            return Err("number of points must be ≥ 3");
-        }
-        if ncol < ndim {
-            return Err("points.ncol must be ≥ ndim");
-        }
-        if nnode < ndim + 1 {
-            return Err("number of cell nodes (nnode) must be ≥ ndim + 1");
-        }
+        large_cutoff: Option<f64>,
+        large_pct: Option<f64>,
+    ) -> Result<Self, StrError> {
+        // constants
+        let ndim = mesh.ndim;
+        let npoint = mesh.points.len();
+        let ncell = mesh.cells.len();
 
         // tolerance
         let tolerance = match tolerance {
             Some(v) => v,
-            None => GS_DEFAULT_TOLERANCE,
+            None => FIND_CELL_TOLERANCE,
         };
         if tolerance <= 0.0 {
             return Err("tolerance must be > 0.0");
@@ -117,28 +100,28 @@ impl GridSearchCellNew {
         // border tolerance
         let border_tol = match border_tol {
             Some(v) => v,
-            None => GS_DEFAULT_BORDER_TOL,
+            None => FIND_CELL_BORDER_TOL,
         };
         if border_tol < 0.0 {
             return Err("border_tol must be ≥ 0.0");
         }
 
         // large cells cutoff
-        let large_cells_cutoff = match large_cells_cutoff {
+        let large_cells_cutoff = match large_cutoff {
             Some(v) => v,
-            None => GS_LARGE_CELLS_CUTOFF,
+            None => FIND_CELL_LARGE_CUTOFF,
         };
         if large_cells_cutoff < 0.0 || large_cells_cutoff > 1.0 {
-            return Err("large_cells_cutoff must be in [0.0, 1.0]");
+            return Err("large_cutoff must be in [0.0, 1.0]");
         }
 
         // large cells pct
-        let large_cells_pct = match large_cells_pct {
+        let large_cells_pct = match large_pct {
             Some(v) => v,
-            None => GS_LARGE_CELLS_PCT,
+            None => FIND_CELL_LARGE_PCT,
         };
         if large_cells_pct < 0.0 || large_cells_pct > 1.0 {
-            return Err("large_cells_pct must be in [0.0, 1.0]");
+            return Err("large_pct must be in [0.0, 1.0]");
         }
 
         // find limits, bounding boxes, and largest bounding box
@@ -148,12 +131,12 @@ impl GridSearchCellNew {
         let mut x_min_max = vec![vec![0.0; N_MIN_MAX]; ndim];
         let mut bbox_largest = vec![f64::MIN; ndim];
         let mut bounding_boxes = Vec::new();
-        for cell_id in 0..ncell {
-            for m in 0..nnode {
-                let point_id = cells.at(cell_id, m);
+        for cell in &mesh.cells {
+            for m in 0..cell.points.len() {
+                let point_id = cell.points[m];
                 for i in 0..ndim {
                     // coords
-                    x[i] = points.at(point_id, i);
+                    x[i] = mesh.points[point_id].coords[i];
                     // limits
                     xmin[i] = f64::min(xmin[i], x[i]);
                     xmax[i] = f64::max(xmax[i], x[i]);
@@ -252,8 +235,8 @@ impl GridSearchCellNew {
         }
 
         // allocate grid
-        Ok(GridSearchCellNew {
-            ndim,
+        Ok(FindCell {
+            mesh,
             ndiv,
             xmin,
             xmax,
@@ -286,101 +269,36 @@ impl GridSearchCellNew {
 
 #[cfg(test)]
 mod tests {
-    use super::GridSearchCellNew;
+    use super::FindCell;
     use crate::geometry::{in_triangle, triangle_coords};
-    use crate::util::{GS_DEFAULT_BORDER_TOL, GS_DEFAULT_TOLERANCE};
-    use plotpy::Plot;
-
-    #[allow(unused_imports)]
-    use plotpy::{Canvas, PolyCode};
-
-    // DO NOT DELETE the lines below
-    fn draw_triangles(plot: &mut Plot, triangles: &[[[f64; 2]; 3]]) {
-        let mut canvas = Canvas::new();
-        canvas.set_face_color("#fefddc").set_edge_color("#fcb827");
-        for t in 0..triangles.len() {
-            canvas.polycurve_begin();
-            for m in 0..3 {
-                let code = if m == 0 { PolyCode::MoveTo } else { PolyCode::LineTo };
-                canvas.polycurve_add(&triangles[t][m][0], &triangles[t][m][1], code);
-            }
-            canvas.polycurve_end(true);
-        }
-        plot.add(&canvas);
-    }
-
-    // DO NOT DELETE the lines below
-    fn draw_tetrahedra(plot: &mut Plot, tets: &[[[f64; 3]; 4]]) {
-        const EDGES: [(usize, usize); 6] = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)];
-        let mut canvas = Canvas::new();
-        canvas.set_face_color("None").set_edge_color("#fcb827");
-        for t in 0..tets.len() {
-            for (ma, mb) in &EDGES {
-                let xa = &tets[t][*ma];
-                let xb = &tets[t][*mb];
-                canvas.polyline_3d_begin();
-                canvas.polyline_3d_add(xa[0], xa[1], xa[2]);
-                canvas.polyline_3d_add(xb[0], xb[1], xb[2]);
-                canvas.polyline_3d_end();
-            }
-        }
-        plot.add(&canvas);
-    }
+    use crate::mesh::Samples;
 
     #[test]
     fn new_handles_errors() {
-        let points = vec![vec![0.0]];
-        let cells = vec![vec![]];
-        let x = vec![0.0, 0.0, 0.0];
+        let mesh = Samples::one_tri3();
         assert_eq!(
-            GridSearchCellNew::new(1, &points, &cells, None, None, None, None).err(),
-            Some("ndim must be 2 or 3")
-        );
-        assert_eq!(
-            GridSearchCellNew::new(2, &points, &cells, None, None, None, None).err(),
-            Some("number of points must be ≥ 3")
-        );
-        let points = vec![vec![0.0, 0.0], vec![1.0, 1.0], vec![2.0, 2.0]];
-        assert_eq!(
-            GridSearchCellNew::new(3, &points, &cells, None, None, None, None).err(),
-            Some("points.ncol must be ≥ ndim")
-        );
-        let points = vec![vec![0.0], vec![1.0], vec![2.0]];
-        assert_eq!(
-            GridSearchCellNew::new(2, &points, &cells, None, None, None, None).err(),
-            Some("points.ncol must be ≥ ndim")
-        );
-        let points = vec![vec![0.0, 0.0], vec![1.0, 0.0], vec![0.0, 1.0]];
-        let cells = vec![vec![1, 2], vec![0, 1, 2], vec![0, 1, 2]];
-        assert_eq!(
-            GridSearchCellNew::new(2, &points, &cells, None, None, None, None).err(),
-            Some("number of cell nodes (nnode) must be ≥ ndim + 1")
-        );
-        let points = vec![vec![0.0, 0.0], vec![1.0, 0.0], vec![0.0, 1.0]];
-        let cells = vec![vec![0, 1, 2], vec![0, 1, 2], vec![0, 1, 2]];
-        assert_eq!(
-            GridSearchCellNew::new(2, &points, &cells, Some(-1.0), None, None, None).err(),
+            FindCell::new(&mesh, Some(-1.0), None, None, None).err(),
             Some("tolerance must be > 0.0")
         );
         assert_eq!(
-            GridSearchCellNew::new(2, &points, &cells, None, Some(-1.0), None, None).err(),
+            FindCell::new(&mesh, None, Some(-1.0), None, None).err(),
             Some("border_tol must be ≥ 0.0")
         );
         assert_eq!(
-            GridSearchCellNew::new(2, &points, &cells, None, None, Some(-1.0), None).err(),
-            Some("large_cells_cutoff must be in [0.0, 1.0]")
+            FindCell::new(&mesh, None, None, Some(-1.0), None).err(),
+            Some("large_cutoff must be in [0.0, 1.0]")
         );
         assert_eq!(
-            GridSearchCellNew::new(2, &points, &cells, None, None, Some(2.0), None).err(),
-            Some("large_cells_cutoff must be in [0.0, 1.0]")
+            FindCell::new(&mesh, None, None, Some(2.0), None).err(),
+            Some("large_cutoff must be in [0.0, 1.0]")
         );
         assert_eq!(
-            GridSearchCellNew::new(2, &points, &cells, None, None, None, Some(-1.0)).err(),
-            Some("large_cells_pct must be in [0.0, 1.0]")
+            FindCell::new(&mesh, None, None, None, Some(-1.0)).err(),
+            Some("large_pct must be in [0.0, 1.0]")
         );
         assert_eq!(
-            GridSearchCellNew::new(2, &points, &cells, None, None, None, Some(2.0)).err(),
-            Some("large_cells_pct must be in [0.0, 1.0]")
+            FindCell::new(&mesh, None, None, None, Some(2.0)).err(),
+            Some("large_pct must be in [0.0, 1.0]")
         );
     }
 }
