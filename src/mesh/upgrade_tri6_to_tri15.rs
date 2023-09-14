856cc47ecf5f1a72946b84fa53ade27dbb575967
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-09-14 18:53:06 +1000

    [wip] Improve upgrade mesh function (quads are possible now)

--- src/mesh/upgrade_tri6_to_tri15.rs
+++ src/mesh/upgrade_tri6_to_tri15.rs
@@ -1,10 +1,10 @@
 use super::{Extract, Features, Mesh, Point};
-use crate::shapes::{GeoKind, Scratchpad, Tri15, Tri6};
+use crate::shapes::{GeoClass, GeoKind, Scratchpad};
 use crate::StrError;
 use russell_lab::Vector;
 
-/// Converts a mesh with only Tri6 cells to a mesh with Tri15 cells
-pub fn upgrade_tri6_to_tri15(mesh: &mut Mesh) -> Result<(), StrError> {
+/// Upgrades a mesh with triangles or quadrilaterals to a higher order
+pub fn upgrade_tri_mesh(mesh: &mut Mesh, target: GeoKind) -> Result<(), StrError> {
     //        2,
     //  s     | ',
     //        |   ',
@@ -20,7 +20,7 @@ pub fn upgrade_tri6_to_tri15(mesh: &mut Mesh) -> Result<(), StrError> {
     //        0-----6-----3-----7-----1  edge of one triangle will be the
     //        1-----7-----3-----6-----0  second point along the edge of the
     //  2     |      side = 0       ,'   neighbor triangle, and vice-versa
-    //        |                   ,'
+    //        |                   ,'     Solution: Flip Normals
     //  =     8    13    12    11'
     //        |               ,'
     //  e     |             ,'
@@ -31,27 +31,54 @@ pub fn upgrade_tri6_to_tri15(mesh: &mut Mesh) -> Result<(), StrError> {
     //        |   ,'
     //  s     | ,'
     //        2'
+    if mesh.ndim != 2 {
+        return Err("mesh ndim must be equal to 2");
+    }
 
     // current number of points and cells
     let npoint = mesh.points.len();
     let ncell = mesh.cells.len();
+    if ncell < 1 {
+        return Err("the conversion requires at least one cell");
+    }
 
-    // 1 + max possible number of points (neglecting overlap)
-    // this number will indicate that a point has not been set yet
-    // if there is only one Tri6 cell, not_set_yet will be equal to 16
-    let not_set_yet = 1 + npoint + ncell * 9;
+    // get info about first cell in the mesh
+    let source = mesh.cells[0].kind;
+    let source_nnode = source.nnode();
+    let target_nnode = target.nnode();
+    let delta_nnode = target_nnode - source_nnode;
+    if target.class() != source.class() {
+        return Err("target class must equal the GeoClass of current cells");
+    }
+    if !(target.class() == GeoClass::Tri || target.class() == GeoClass::Qua) {
+        return Err("target GeoClass must be Tri or Qua");
+    }
+    if delta_nnode < 1 {
+        return Err("target GeoKind must have more nodes than the current GeoKind");
+    }
+    let nedge = source.nedge();
+    let source_edge_nnode = source.edge_nnode();
+    let target_edge_nnode = target.edge_nnode();
+    assert!(target_edge_nnode >= source_edge_nnode);
+
+    // not_set_yet: number that indicates that a point has not been set yet
+    // here we use 1 + max number of points possible, ignoring edge overlaps
+    // for example, in a mesh with two Tri6 cells (ncell = 2, npoint = 9), if target = Tri15,
+    // then delta_nnode = 9, and not_set_yet will be equal to 1 + 9 + 2 * 9 = 28,
+    // however, the final total number of points will be 25
+    let not_set_yet = 1 + npoint + ncell * delta_nnode;
 
     // expand connectivity array
     for i in 0..ncell {
-        if mesh.cells[i].kind != GeoKind::Tri6 {
-            return Err("only Tri6 cells are allowed in the conversion");
+        if mesh.cells[i].kind != source {
+            return Err("all current cells must have the same GeoKind");
         }
-        mesh.cells[i].points.resize(15, not_set_yet);
+        mesh.cells[i].points.resize(target_nnode, not_set_yet);
     }
 
     // scratchpad for point interpolation
     const NDIM: usize = 2;
-    let mut pad = Scratchpad::new(NDIM, GeoKind::Tri6).unwrap();
+    let mut pad = Scratchpad::new(NDIM, source).unwrap();
 
     // real coordinates of new points
     let mut x = Vector::new(NDIM);
@@ -60,13 +87,13 @@ pub fn upgrade_tri6_to_tri15(mesh: &mut Mesh) -> Result<(), StrError> {
     let features = Features::new(mesh, Extract::All);
     let edges = features.all_2d_edges.unwrap();
 
+    // get info about target
+    let target_first_interior_node = target_nnode - target.n_interior_nodes();
+
     // perform the conversion
     for this_cell_id in 0..ncell {
-        // access this cell (opposite to neighbor cell)
-        // let this_cell = &mut mesh.cells[this_cell_id];
-
-        // set coordinates matrix of Tri6
-        for m in 0..6 {
+        // set coordinates matrix of the source kind
+        for m in 0..source_nnode {
             let p = mesh.cells[this_cell_id].points[m];
             let x = mesh.points[p].coords[0];
             let y = mesh.points[p].coords[1];
@@ -74,67 +101,69 @@ pub fn upgrade_tri6_to_tri15(mesh: &mut Mesh) -> Result<(), StrError> {
             pad.set_xx(m, 1, y);
         }
 
-        // new points on the edge of the Tri15
-        for m in 6..12 {
-            let current_new_point = mesh.cells[this_cell_id].points[m];
-            if current_new_point == not_set_yet {
-                // id and marker of middle point
-                let local_mid = m / 2; // 3 3  4 4  5 5
-                let mid_point = mesh.cells[this_cell_id].points[local_mid];
-                let marker = mesh.points[mid_point].marker;
+        // new points on the edge of the target kind
+        if target_edge_nnode > source_edge_nnode {
+            for e in 0..nedge {
+                for i in 2..target_edge_nnode {
+                    // local point on the edge of target
+                    let m = target.edge_node_id_inward(e, i); // order => inward normals
 
-                // coordinates of new point
-                pad.calc_coords(&mut x, &Tri15::NODE_REFERENCE_COORDS[m]).unwrap();
+                    let current_new_point = mesh.cells[this_cell_id].points[m];
+                    if current_new_point == not_set_yet {
+                        // use marker of the middle point, if any
+                        let marker = if source_edge_nnode > 2 {
+                            let local_mid = source.edge_node_id_inward(e, 2);
+                            let mid_point = mesh.cells[this_cell_id].points[local_mid];
+                            mesh.points[mid_point].marker
+                        } else {
+                            0
+                        };
 
-                // new point
-                let new_point_id = mesh.points.len();
-                mesh.points.push(Point {
-                    id: new_point_id,
-                    marker,
-                    coords: x.as_data().clone(),
-                });
+                        // coordinates of new point
+                        pad.calc_coords(&mut x, target.reference_coords(m)).unwrap();
 
-                // set cell point
-                mesh.cells[this_cell_id].points[m] = new_point_id;
+                        // new point
+                        let new_point_id = mesh.points.len();
+                        mesh.points.push(Point {
+                            id: new_point_id,
+                            marker,
+                            coords: x.as_data().clone(),
+                        });
 
-                // parameters
-                // m         = 6 7  8 9 10 11
-                // m - 6     = 0 1  2 3  4 5
-                // this_side =  0    1    2   = (m - 6) / 2
-                // this_pos  = 0 1  0 1  0 1  = (m - 6) % 2
-                // neigh_pos = 1 0  1 0  1 0  = 1 - this_pos
-                let this_side = (m - 6) / 2; // side of the new vertex on this triangle
-                let this_pos = (m - 6) % 2; // 0 or 1 (first or second node on the side of this triangle)
-                let neigh_pos = 1 - this_pos; // 1 or 0 (second or first node on neighbor's side)
+                        // set cell point
+                        mesh.cells[this_cell_id].points[m] = new_point_id;
 
-                // find neighboring triangle and the side of edge on the neighbor triangle
-                let local_a = Tri6::EDGE_NODE_IDS[this_side][0];
-                let local_b = Tri6::EDGE_NODE_IDS[this_side][1];
-                let mut this_a = mesh.cells[this_cell_id].points[local_a];
-                let mut this_b = mesh.cells[this_cell_id].points[local_b];
-                if this_b < this_a {
-                    let temp = this_a;
-                    this_a = this_b;
-                    this_b = temp;
+                        // search cells sharing this edge
+                        let local_a = source.edge_node_id(e, 0);
+                        let local_b = source.edge_node_id(e, 1);
+                        let mut this_a = mesh.cells[this_cell_id].points[local_a];
+                        let mut this_b = mesh.cells[this_cell_id].points[local_b];
+                        if this_b < this_a {
+                            let temp = this_a;
+                            this_a = this_b;
+                            this_b = temp;
+                        }
+                        let edge_shares = edges.get(&(this_a, this_b)).unwrap();
+                        let shares: Vec<_> = edge_shares.iter().filter(|v| v.0 != this_cell_id).collect();
+
+                        // found neighbor
+                        if shares.len() == 1 {
+                            let neigh_cell_id = shares[0].0;
+                            let neigh_edge_id = shares[0].1;
+                            let n = target.edge_node_id(neigh_edge_id, i); // order => outward normals
+
+                            // set new point of neighbor
+                            mesh.cells[neigh_cell_id].points[n] = new_point_id;
+                        }
+                    }
                 }
-                let edge_shares = edges.get(&(this_a, this_b)).unwrap();
-                let shares: Vec<_> = edge_shares.iter().filter(|v| v.0 != this_cell_id).collect();
-                if shares.len() == 1 {
-                    // n is the index of new point on neighbor's edge
-                    let neigh_cell_id = shares[0].0;
-                    let neigh_side = shares[0].1;
-                    let n = 6 + neigh_side * 2 + neigh_pos;
-
-                    // set new point of neighbor
-                    mesh.cells[neigh_cell_id].points[n] = new_point_id;
-                } // else => the edge is on the boundary (shared by one triangle only)
             }
         }
 
-        // new points in the interior of the Tri15
-        for m in 12..15 {
+        // new points in the interior
+        for m in target_first_interior_node..target_nnode {
             // coordinates of new point
-            pad.calc_coords(&mut x, &Tri15::NODE_REFERENCE_COORDS[m]).unwrap();
+            pad.calc_coords(&mut x, target.reference_coords(m)).unwrap();
 
             // new point
             let new_point_id = mesh.points.len();
@@ -149,7 +178,7 @@ pub fn upgrade_tri6_to_tri15(mesh: &mut Mesh) -> Result<(), StrError> {
         }
 
         // set new geo code
-        mesh.cells[this_cell_id].kind = GeoKind::Tri15;
+        mesh.cells[this_cell_id].kind = target;
     }
     Ok(())
 }
@@ -158,11 +187,10 @@ pub fn upgrade_tri6_to_tri15(mesh: &mut Mesh) -> Result<(), StrError> {
 
 #[cfg(test)]
 mod tests {
-    use russell_chk::vec_approx_eq;
-
-    use super::upgrade_tri6_to_tri15;
+    use super::upgrade_tri_mesh;
     use crate::mesh::{check_all, Cell, Mesh, Point};
     use crate::shapes::GeoKind;
+    use russell_chk::vec_approx_eq;
 
     #[allow(unused_imports)]
     use crate::mesh::draw_mesh;
@@ -173,19 +201,24 @@ mod tests {
     fn upgrade_tri6_to_tri15_captures_errors() {
         #[rustfmt::skip]
         let mut mesh = Mesh {
-            ndim: 2,
+            ndim: 3,
             points: vec![
-                Point { id: 0, marker: 0, coords: vec![0.0, 0.0 ] },
-                Point { id: 1, marker: 0, coords: vec![1.0, 0.0 ] },
-                Point { id: 2, marker: 0, coords: vec![0.5, 0.85] },
+                Point { id: 0, marker: 0, coords: vec![0.0, 0.0, 0.0] },
+                Point { id: 1, marker: 0, coords: vec![1.0, 0.0, 0.0] },
+                Point { id: 2, marker: 0, coords: vec![1.0, 1.0, 0.0] },
+                Point { id: 3, marker: 0, coords: vec![0.0, 1.0, 0.0] },
+                Point { id: 4, marker: 0, coords: vec![0.0, 0.0, 1.0] },
+                Point { id: 5, marker: 0, coords: vec![1.0, 0.0, 1.0] },
+                Point { id: 6, marker: 0, coords: vec![1.0, 1.0, 1.0] },
+                Point { id: 7, marker: 0, coords: vec![0.0, 1.0, 1.0] },
             ],
             cells: vec![
-                Cell { id: 0, attribute: 1, kind: GeoKind::Tri3, points: vec![0, 1, 2] },
+                Cell { id: 0, attribute: 1, kind: GeoKind::Hex8, points: vec![0,1,2,3, 4,5,6,7] },
             ],
         };
         assert_eq!(
-            upgrade_tri6_to_tri15(&mut mesh).err(),
-            Some("only Tri6 cells are allowed in the conversion")
+            upgrade_tri_mesh(&mut mesh, GeoKind::Tri15).err(),
+            Some("mesh ndim must be equal to 2")
         );
     }
 
@@ -216,7 +249,7 @@ mod tests {
             draw_mesh(&mesh, true, true, false, "/tmp/gemlab/test_tri6_to_tri15_1_before.svg").unwrap();
         }
 
-        upgrade_tri6_to_tri15(&mut mesh).unwrap();
+        upgrade_tri_mesh(&mut mesh, GeoKind::Tri15).unwrap();
 
         if SAVE_FIGURE {
             draw_mesh(&mesh, true, true, false, "/tmp/gemlab/test_tri6_to_tri15_1_after.svg").unwrap();
