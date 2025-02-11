2a2b16b147d7d4790729fdc381ce73f757e98606
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-02-11 20:11:00 +1000

    Improve connected_edges testing

--- src/mesh/edges.rs
+++ src/mesh/edges.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 
 use super::Edges;
+use crate::StrError;
 
 impl<'a> Edges<'a> {
     /// Returns indices of edges in connected order
@@ -29,9 +30,9 @@ impl<'a> Edges<'a> {
     /// let path = edges.connected_path().unwrap();
     /// assert_eq!(path, vec![0, 1]); // Indices into edges.all
     /// ```
-    pub fn connected_path(&self) -> Option<Vec<usize>> {
+    pub fn connected_path(&self) -> Result<Vec<usize>, StrError> {
         if self.all.is_empty() {
-            return Some(Vec::new());
+            return Err("the edges list is empty");
         }
 
         let mut path = Vec::with_capacity(self.all.len());
@@ -54,7 +55,7 @@ impl<'a> Edges<'a> {
                     if a == c || a == d || b == c || b == d {
                         if found {
                             // More than one connection means there's a branch
-                            return None;
+                            return Err("found branching");
                         }
                         current = i;
                         path.push(current);
@@ -71,10 +72,10 @@ impl<'a> Edges<'a> {
 
         // All edges must be connected
         if path.len() != self.all.len() {
-            return None;
+            return Err("found disconnected edges");
         }
 
-        Some(path)
+        Ok(path)
     }
 }
 
@@ -90,18 +91,22 @@ mod tests {
 
     #[rustfmt::skip]
     fn generate_sample() -> Vec<Edge> {
-        //   9--------7-------.4--------6
-        //   |        |     .' |        |
-        //   |        |   .'   |        |
-        //   |        | .'     |        |
-        //   1--------8'       |        |
-        //   |        |        |        |
-        //   |        |        |        |
-        //   |        |        |        |
-        //   5--------2--------3-------10
-
-        // IMPORTANT: The diagram above may not work as a FEM mesh because
-        // the outward normals may not be pointing in the right direction.
+        //          (11)           (12)           (13)
+        //    9--------------7-------------.4--------------6
+        //    |              |           .' |              |
+        //    |              |         .'   |              |
+        // (4)|           (6)|   (7) .'     |              |
+        //    |              |     .'       |              |
+        //    |              |   .'         |              |
+        //    |      (8)     | .'           |(9)           |(10)
+        //    1--------------8'             |              |
+        //    |              |              |              |
+        //    |              |              |              |
+        // (3)|           (5)|              |              |
+        //    |              |              |              |
+        //    |              |              |              |
+        //    5--------------2--------------3-------------10
+        //           (0)           (1)            (2)
 
         // Bottom horizontal edges
         let e0 = Edge { kind: GeoKind::Lin2, points: vec![5, 2] };
@@ -112,76 +117,49 @@ mod tests {
         let e3 = Edge { kind: GeoKind::Lin2, points: vec![5, 1] };
         let e4 = Edge { kind: GeoKind::Lin2, points: vec![1, 9] };
 
-        // Middle vertical and diagonal edges
+        // Central edges
         let e5 = Edge { kind: GeoKind::Lin2, points: vec![2, 8] };
         let e6 = Edge { kind: GeoKind::Lin2, points: vec![8, 7] };
         let e7 = Edge { kind: GeoKind::Lin2, points: vec![8, 4] };
+        let e8 = Edge { kind: GeoKind::Lin2, points: vec![1, 8] };
 
         // Right vertical edges
-        let e8 = Edge { kind: GeoKind::Lin2, points: vec![3, 4] };
-        let e9 = Edge { kind: GeoKind::Lin2, points: vec![10, 6] };
+        let e9  = Edge { kind: GeoKind::Lin2, points: vec![3, 4] };
+        let e10 = Edge { kind: GeoKind::Lin2, points: vec![10, 6] };
 
         // Top horizontal edges
-        let e10 = Edge { kind: GeoKind::Lin2, points: vec![9, 7] };
-        let e11 = Edge { kind: GeoKind::Lin2, points: vec![7, 4] };
-        let e12 = Edge { kind: GeoKind::Lin2, points: vec![4, 6] };
+        let e11 = Edge { kind: GeoKind::Lin2, points: vec![9, 7] };
+        let e12 = Edge { kind: GeoKind::Lin2, points: vec![7, 4] };
+        let e13 = Edge { kind: GeoKind::Lin2, points: vec![4, 6] };
 
-        vec![e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12]
-    }
-
-    #[rustfmt::skip]
-    fn generate_sample_o2() -> Vec<Edge> {
-        //   9---22---7---25--.4---14---6
-        //   |        |     .' |        |
-        //  23       24   16   |        |
-        //   |        | .'     |        |
-        //   1---21---8'      12       11
-        //   |        |        |        |
-        //  19       18        |        |
-        //   |        |        |        |
-        //   5---20---2---17---3---13--10
-
-        // IMPORTANT: The diagram above may not work as a FEM mesh because
-        // the outward normals may not be pointing in the right direction.
-
-        // Bottom horizontal edges
-        let e0 = Edge { kind: GeoKind::Lin3, points: vec![5, 2, 20] };
-        let e1 = Edge { kind: GeoKind::Lin3, points: vec![2, 3, 17] };
-        let e2 = Edge { kind: GeoKind::Lin3, points: vec![3, 10, 13] };
-
-        // Left vertical edges
-        let e3 = Edge { kind: GeoKind::Lin3, points: vec![5, 1, 19] };
-        let e4 = Edge { kind: GeoKind::Lin3, points: vec![1, 9, 23] };
-
-        // Middle vertical and diagonal edges
-        let e5 = Edge { kind: GeoKind::Lin3, points: vec![2, 8, 18] };
-        let e6 = Edge { kind: GeoKind::Lin3, points: vec![8, 7, 24] };
-        let e7 = Edge { kind: GeoKind::Lin3, points: vec![8, 4, 16] };
-
-        // Right vertical edges
-        let e8 = Edge { kind: GeoKind::Lin3, points: vec![3, 4, 12] };
-        let e9 = Edge { kind: GeoKind::Lin3, points: vec![10, 6, 11] };
-
-        // Top horizontal edges
-        let e10 = Edge { kind: GeoKind::Lin3, points: vec![9, 7, 22] };
-        let e11 = Edge { kind: GeoKind::Lin3, points: vec![7, 4, 25] };
-        let e12 = Edge { kind: GeoKind::Lin3, points: vec![4, 6, 14] };
-
-        vec![e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12]
+        vec![e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13]
     }
 
     #[test]
     fn test_connected_edges_path_1() {
-        //   9--------7-------.4--------6
-        //   |        |     .' |        |
-        //   |        |   .'   |        |
-        //   |        | .'     |        |
-        //   1--------8'       |        |
-        //   |        |        |        |
-        //   |        |        |        |
-        //   |        |        |        |
-        //   5--------2--------3-------10
         let all = generate_sample();
-        // TODO
+
+        let empty = Edges { all: vec![] };
+        assert_eq!(empty.connected_path().err(), Some("the edges list is empty"));
+
+        let branching = Edges {
+            all: vec![&all[5], &all[6], &all[7], &all[8]],
+        };
+        assert_eq!(branching.connected_path().err(), Some("found branching"));
+
+        let disconnected = Edges {
+            all: vec![&all[3], &all[4], &all[10]],
+        };
+        assert_eq!(disconnected.connected_path().err(), Some("found disconnected edges"));
+
+        let bottom = Edges {
+            all: vec![&all[0], &all[1], &all[2]],
+        };
+        assert_eq!(bottom.connected_path().unwrap(), vec![0, 1, 2]);
+
+        let loop1 = Edges {
+            all: vec![&all[5], &all[7], &all[9], &all[1]],
+        };
+        assert_eq!(loop1.connected_path().unwrap(), vec![0, 1, 2, 3]);
     }
 }
