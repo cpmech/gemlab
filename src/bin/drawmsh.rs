d4085ef80c0eff35903f20e18ec60f783b54efd1
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-11-15 10:26:58 +1000

    Impl drawmsh tool

---
+++ src/bin/drawmsh.rs
@@ -0,0 +1,63 @@
+use gemlab::mesh::{Figure, Mesh};
+use gemlab::StrError;
+use std::path::Path;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+const OUT_DIR: &str = "/tmp/gemlab";
+
+/// Command line options
+#[derive(Debug, StructOpt)]
+#[structopt(name = "drawmsh", about = "Draw MSH file")]
+struct Options {
+    /// Input MSH file
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+
+    /// Show points in the SVG figure
+    #[structopt(short = "p", long)]
+    show_points: bool,
+
+    /// Show IDs of points in the SVG figure
+    #[structopt(short = "v", long)]
+    show_point_ids: bool,
+
+    /// Show IDs of cells in the SVG figure
+    #[structopt(short = "c", long)]
+    show_cell_ids: bool,
+
+    #[structopt(long)]
+    fig_width: Option<f64>,
+
+    #[structopt(long)]
+    fig_height: Option<f64>,
+}
+
+fn main() -> Result<(), StrError> {
+    // parse options
+    let options = Options::from_args();
+
+    // extract file stem
+    let in_path = Path::new(&options.input);
+    let fn_stem = in_path
+        .file_stem()
+        .ok_or("cannot get file stem")?
+        .to_str()
+        .ok_or("cannot convert file stem to str")?;
+
+    // load MSH file
+    let mesh = Mesh::read(in_path)?;
+
+    // write SVG file
+    let mut fig = Figure::new();
+    fig.show_point_dots(options.show_points)
+        .show_point_ids(options.show_point_ids)
+        .show_cell_ids(options.show_cell_ids)
+        .size(
+            options.fig_width.unwrap_or(1000.0),
+            options.fig_height.unwrap_or(1000.0),
+        );
+    fig.draw(&mesh, &format!("{}/{}.svg", OUT_DIR, fn_stem))?;
+    println!("Generated SVG file: {}/{}.svg", OUT_DIR, fn_stem);
+    Ok(())
+}
---
+++ zscripts/run-drawmsh.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cargo run --bin drawmsh -- data/meshes/rectangle_tris_quads.msh -p -v -c
