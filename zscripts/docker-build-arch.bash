d32e8a8ed256588ea374b46d0a794a2a0608dfec
Dorival Pedroso <dorival.pedroso@gmail.com>
2026-06-08 17:19:14 +1000

    Add GH Action to test with IntelMKL and Local Libs

--- zscripts/docker-build-arch.bash
+++ zscripts/docker-build-arch.bash
@@ -2,8 +2,16 @@
 
 set -euo pipefail
 
-DOCKERFILE="zdocker/Dockerfile.Arch"
-NAME="cpmech/gemlab_arch"
+ALTERNATIVE="${1:-0}"
+
+# image name and Dockerfile
+if [ "${ALTERNATIVE}" = "1" ]; then
+    DOCKERFILE="zdocker/Dockerfile.Arch.Mkl.Local"
+    NAME="cpmech/gemlab_arch_mkl_local"
+else
+    DOCKERFILE="zdocker/Dockerfile.Arch"
+    NAME="cpmech/gemlab_arch"
+fi
 
 # build Docker image
 docker build \
