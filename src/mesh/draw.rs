59fa9d69fdd00d20e092f9a4392ac6381f71eb8e
Dorival Pedroso <dorival.pedroso@gmail.com>
2026-09-24 07:19:18 +1000

    Update figures

--- src/mesh/draw.rs
+++ src/mesh/draw.rs
@@ -1229,7 +1229,7 @@ mod tests {
     use crate::mesh::{Features, Mesh, Samples};
     use plotpy::{Canvas, Plot, Text};
 
-    const SAVE_FIGURE: bool = true;
+    const SAVE_FIGURE: bool = false;
 
     fn labels_and_caption() -> (Text, Text) {
         // labels for cell local ids
